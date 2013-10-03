6d924eb71c2ae1f7f1027dced8d909f808bb5864
Erich Duda <dudaerich@gmail.com>
2013-10-03 15:42:07 +0200

    Set up solr driver in VNF

--- module/VNF/config/module.config.php
+++ module/VNF/config/module.config.php
@@ -1,6 +1,28 @@
 <?php
 namespace VNF\Module\Configuration;
 
-$config = array();
+$config = array(
+    'vufind' => array(
+	    'plugin_managers' => array (
+			'recorddriver' => array (
+				'factories' => array(
+					'solrmkp' => function ($sm) {
+					    $driver = new \VuFind\RecordDriver\SolrMarc(
+					    		$sm->getServiceLocator()->get('VuFind\Config')->get('config'),
+					    		null,
+					    		$sm->getServiceLocator()->get('VuFind\Config')->get('searches')
+					    );
+					    $driver->attachILS(
+					    		$sm->getServiceLocator()->get('VuFind\ILSConnection'),
+					    		$sm->getServiceLocator()->get('VuFind\ILSHoldLogic'),
+					    		$sm->getServiceLocator()->get('VuFind\ILSTitleHoldLogic')
+					    );
+					    return $driver;
+					} /* solrmkp */
+				) /* factories */
+			) /* recorddriver */
+		) /* plugin_managers */
+	) /* vufind */
+);
 
 return $config;
