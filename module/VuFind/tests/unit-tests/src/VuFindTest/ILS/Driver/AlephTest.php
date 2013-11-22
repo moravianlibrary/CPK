796fd2acf34383e6b6b27bda435ae51ec6c21fef
Michal Merta <merta.m@gmail.com>
2013-11-22 13:47:39 +0100

    added HttpService mocking support to Aleph test

--- module/VuFind/tests/unit-tests/src/VuFindTest/ILS/Driver/AlephTest.php
+++ module/VuFind/tests/unit-tests/src/VuFindTest/ILS/Driver/AlephTest.php
@@ -27,6 +27,10 @@
  */
 namespace VuFindTest\ILS\Driver;
 use VuFind\ILS\Driver\Aleph;
+use VuFindHttp\HttpServiceInterface;
+use \Zend\Http\Response;
+
+use RuntimeException;
 
 /**
  * ILS driver test
@@ -39,11 +43,87 @@ use VuFind\ILS\Driver\Aleph;
  */
 class AlephTest extends \VuFindTest\Unit\ILSDriverTestCase
 {
+    
+    protected $mockedHttpService;
+    protected $driverConfig;
+    
     /**
      * Constructor
      */
     public function __construct()
     {
         $this->driver = new Aleph(new \VuFind\Date\Converter());
+        $this->mockedHttpService = $this->getMock('VuFindHttp\HttpServiceInterface');
+  
+        $this->driver->setHttpService($this->mockedHttpService);
+        $this->driverConfig = $this->getDriverConfig('Aleph','2.0');
+    }
+    
+    public function setUp() {
+        $this->driver->setConfig($this->driverConfig);
+        $this->driver->init();
+    }
+    
+    public function testMissingConfiguration()
+    {
+        $this->driver->setConfig(null);
+        $this->setExpectedException('VuFind\Exception\ILS');
+        $this->driver->init();
+    }
+    
+    public function testGetHoldingInfoForItem()
+    {
+        $response = new Response();
+        $response->setContent($this->getResponse('aleph', 'response_700.xml'));
+        $response->setStatusCode(Response::STATUS_CODE_200);
+        $response->setReasonPhrase('OK');
+        
+        $this->mockedHttpService->expects($this->any())->method('get')
+                ->with($this->equalTo("http://aleph.mylibrary.edu:1892/rest-dlf/patron/700/record/LIB01MZK01000748028/items/MZK50000748028000010"),
+                    $this->anything(),$this->anything())
+                ->will($this->returnValue($response));
+        
+        $this->driver->getHoldingInfoForItem('700', 'MZK01000748028', 'MZK50000748028000010');
+    } 
+
+    /**
+     * Retrieve configuration from /fixtures/configs/ @param $version / @param $driver
+     * @param string $driver
+     * @param string $version
+     * @throws RuntimeException
+     * @return array
+     */
+    protected function getDriverConfig($driver, $version = '2.0')
+    {
+        $file = realpath(
+                \VUFIND_PHPUNIT_MODULE_PATH . '/fixtures/configs/'.$version.'/'.$driver.'.ini'
+        );
+        if (!$file) {
+            throw new RuntimeException(
+                    sprintf('Unable to get configuration for driver %s', $driver)
+            );
+        }
+        $config = parse_ini_file($file,true);
+        return $config;
+    }
+    
+    /**
+     * Retrieve configuration from /fixtures/response/ @param $type / @param $name
+     * @param string $type
+     * @param string $name
+     * @throws RuntimeException
+     * @return string
+     */
+    protected function getResponse($type, $name) {
+        $file = realpath(
+                \VUFIND_PHPUNIT_MODULE_PATH . '/fixtures/response/'.$type.'/'.$name
+        );
+        if (!$file) {
+            throw new RuntimeException(
+                    sprintf('Unable to resolve fixture to fixture file: %s', $name)
+            );
+        }
+        $response = file_get_contents($file);
+        return $response;
     }
 }
