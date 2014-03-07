8127feb21f420e06b30d2c6c8e88b60a23829236
Vaclav Rosecky <xrosecky@gmail.com>
2014-03-07 15:12:53 +0100

    support for citace.com

--- module/MZKCatalog/config/module.config.php
+++ module/MZKCatalog/config/module.config.php
@@ -9,7 +9,31 @@ $config = array(
                     'solrmzk' => 'MZKCatalog\RecordDriver\Factory::getSolrMarc',
                 ) /* factories */
             ), /* recorddriver */
+            'recordtab' => array(
+                'abstract_factories' => array('VuFind\RecordTab\PluginFactory'),
+                'invokables' => array(
+                    'holdingsils' => 'MZKCommon\RecordTab\HoldingsILS',
+                    'citation' => 'MZKCatalog\RecordTab\Citation',
+                ), /* invokables */
+            ), /* recordtab */
         ), /* plugin_managers */
+        'recorddriver_tabs' => array(
+            'MZKCatalog\RecordDriver\SolrMarc' => array(
+                'tabs' => array(
+                    'Holdings' => 'HoldingsILS',
+                    'Description' => 'Description',
+                    'Citation' => 'Citation',
+                    'TOC' => 'TOC',
+                    'UserComments' => 'UserComments',
+                    'Reviews' => 'Reviews',
+                    'Excerpt' => 'Excerpt',
+                    'HierarchyTree' => 'HierarchyTree',
+                    'Map' => 'Map',
+                    'Details' => 'StaffViewMARC',
+                ),
+                'defaultTab' => null,
+            ),
+        ) /* recorddriver_tabs */
     ), /* vufind */
 );
 
