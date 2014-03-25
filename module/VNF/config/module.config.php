5457be3688da0e06cb2b4ddd2626fd104219d5dc
Vaclav Rosecky <xrosecky@gmail.com>
2014-03-25 14:45:07 +0100

    default record driver for VNF

--- module/VNF/config/module.config.php
+++ module/VNF/config/module.config.php
@@ -3,14 +3,15 @@ namespace VNF\Module\Configuration;
 
 $config = array(
     'vufind' => array(
-	    'plugin_managers' => array (
-			'recorddriver' => array (
-				'factories' => array(
-					'solrmkp' => 'VNF\RecordDriver\Factory::getSolrMkp',
-				) /* factories */
-			) /* recorddriver */
-		) /* plugin_managers */
-	) /* vufind */
+        'plugin_managers' => array (
+            'recorddriver' => array (
+                'factories' => array(
+                    'solrmkp'     => 'VNF\RecordDriver\Factory::getSolrMkp',
+                    'solrdefault' => 'VNF\RecordDriver\Factory::getSolrMarc',
+                ) /* factories */
+            ) /* recorddriver */
+        ) /* plugin_managers */
+    ) /* vufind */
 );
 
 return $config;
---
+++ module/VNF/src/VNF/RecordDriver/SolrMarc.php
@@ -0,0 +1,12 @@
+<?php
+namespace VNF\RecordDriver;
+use MZKCommon\RecordDriver\SolrMarc As ParentSolrDefault;
+
+/*
+ * Default record driver for VNF
+ *
+ */
+class SolrMarc extends ParentSolrDefault
+{
+
+}
