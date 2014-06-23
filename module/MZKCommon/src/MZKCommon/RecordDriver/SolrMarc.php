b97d2ffdfa0cd8f3fa84e73b0943a94fc90551c1
Michal Merta <merta.m@gmail.com>
2014-06-23 11:29:52 +0200

    getLocalId, getExternalLinks moved to MZKCommon

--- module/MZKCommon/src/MZKCommon/RecordDriver/SolrMarc.php
+++ module/MZKCommon/src/MZKCommon/RecordDriver/SolrMarc.php
@@ -59,4 +59,41 @@ class SolrMarc extends ParentSolrMarc
         
     }
 
+    public function getLocalId() {
+        list($source, $localId) = explode('.', $this->getUniqueID());
+        return $localId;
+    }
+
+   /**
+    * uses setting from config.ini => External links
+    * @return array  [] => [
+    *          [institution] = institution, 
+    *          [url] = external link to catalogue,
+    *          [display] => link to be possibly displayed]
+    *          [id] => local identifier of record
+    *
+    */
+    public function getExternalLinks() {
+
+        list($ins, $id) = explode('.' , $this->getUniqueID());
+        $linkBase = $this->recordConfig->ExternalLinks->$ins;
+        if (empty($linkBase)) {
+            $resultArray[] = array($ins, '');
+        }
+
+        $finalID = $this->getExternalID();
+        print_r($finalID);
+        if (!isset($finalID)) array(array('institution' => $ins, 'url' => '', 'display' => '', 'id' => $id));
+
+        $confEnd  = $ins . '_end';
+        $linkEnd  = $this->recordConfig->ExternalLinks->$confEnd;
+        if (!isset($linkEnd)) $linkEnd = '';
+        $externalLink =  $linkBase . $finalID . $linkEnd;
+        return array(array('institution' => $ins, 'url' => $externalLink, 'display' => $externalLink, 'id' => $id));
+    }
+
+    protected function getExternalID() {
+        return $this->getLocalId();
+    }
+
 }
--- module/MZKPortal/src/MZKPortal/RecordDriver/SolrMarcBase.php
+++ module/MZKPortal/src/MZKPortal/RecordDriver/SolrMarcBase.php
@@ -11,45 +11,8 @@ class SolrMarcBase extends SolrMarc
         return array($source => $id);
     }
 
-    public function getLocalId() {
-        list($source, $localId) = explode('.', $this->getUniqueID());
-        return $localId;
-    }
-
     public function getHoldLink() {
         return null;
-    }
-    
-
-    /**
-    * uses setting from config.ini => External links
-    * @return array  [] => [
-    *          [institution] = institution, 
-    *          [url] = external link to catalogue,
-    *          [display] => link to be possibly displayed]
-    *          [id] => local identifier of record
-    *
-    */
-    public function getExternalLinks() {
-
-        list($ins, $id) = explode('.' , $this->getUniqueID());
-        $linkBase = $this->recordConfig->ExternalLinks->$ins;
-        if (empty($linkBase)) {
-            $resultArray[] = array($ins, '');
-        }
-
-        $finalID = $this->getExternalID();
-        if (!isset($finalID)) array(array('institution' => $ins, 'url' => '', 'display' => '', 'id' => $id));
-
-        $confEnd  = $ins . '_end';
-        $linkEnd  = $this->recordConfig->ExternalLinks->$confEnd;
-        if (!isset($linkEnd)) $linkEnd = '';
-        $externalLink =  $linkBase . $finalID . $linkEnd;
-        return array(array('institution' => $ins, 'url' => $externalLink, 'display' => $externalLink, 'id' => $id));
-    }
-
-    protected function getExternalID() {
-        return '';
     }
 
 }
