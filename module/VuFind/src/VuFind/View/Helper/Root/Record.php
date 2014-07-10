7f72d3b52fb34e370dfffbc0065c965ba02cb98a
Jakub Sestak <sestak.jakub@gmail.com>
2014-07-10 17:23:42 +0200

    removed auto add to cart in Favourites

--- module/VuFind/src/VuFind/View/Helper/Root/Record.php
+++ module/VuFind/src/VuFind/View/Helper/Root/Record.php
@@ -391,10 +391,31 @@ class Record extends AbstractHelper
         $context
             = array('id' => $id, 'count' => $checkboxCount++, 'prefix' => $idPrefix);
         return $this->contextHelper->renderInContext(
-            'record/checkbox.phtml', $context
+            'record/checkboxAutoAddToCart.phtml', $context
         );
     }
 
+    
+    /**
+     * Render an HTML checkbox control for the current record.
+     *
+     * @param string $idPrefix Prefix for checkbox HTML ids
+     *
+     * @return string
+     */
+    public function getCheckboxWithoutAutoAddingToCart($idPrefix = '')
+    {
+    	static $checkboxCount = 0;
+    	$id = $this->driver->getResourceSource() . '|'
+    			. $this->driver->getUniqueId();
+    	$context
+    	= array('id' => $id, 'count' => $checkboxCount++, 'prefix' => $idPrefix);
+    	return $this->contextHelper->renderInContext(
+    			'record/checkbox.phtml', $context
+    	);
+    }
+
+    
     /**
      * Generate a qrcode URL (return false if unsupported).
      *
