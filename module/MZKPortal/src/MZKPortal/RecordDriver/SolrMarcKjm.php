456ba50e0219cad656580e5c90fdaee3d8c74684
mertam <merta.m@gmail.com>
2014-07-24 10:20:08 +0200

    cistbrno - added external links support for KJM

--- module/MZKPortal/src/MZKPortal/RecordDriver/SolrMarcKjm.php
+++ module/MZKPortal/src/MZKPortal/RecordDriver/SolrMarcKjm.php
@@ -4,4 +4,10 @@ namespace MZKPortal\RecordDriver;
 class SolrMarcKjm extends SolrMarcBase
 {
 
+    protected function getExternalID ()
+    {
+        $localId = $this->getLocalId();
+        $id = substr($localId, 6);
+        return ltrim($id, '0');
+    }
 }
