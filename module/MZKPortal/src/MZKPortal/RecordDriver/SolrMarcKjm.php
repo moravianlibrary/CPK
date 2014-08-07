59ab69e9d341eb6ceed9be7d48a371de16fa7735
mertam <merta.m@gmail.com>
2014-08-07 14:17:38 +0200

    cistbrno - fixed field 993 parsing for KJM

--- module/MZKPortal/src/MZKPortal/RecordDriver/SolrMarcKjm.php
+++ module/MZKPortal/src/MZKPortal/RecordDriver/SolrMarcKjm.php
@@ -26,10 +26,20 @@ class SolrMarcKjm extends SolrMarcBase
             foreach ($currentField->getSubfields(null) as $currentSubfield) {
                 $currentHolding[$currentSubfield->getCode()] = $currentSubfield->getData();
             }
+            
+            //fix ~ subfield recognition
+            if (array_key_exists('l', $currentHolding) && preg_match('/.*\$~.*/', $currentHolding['l'])) {
+                list($l,$tilde) = explode('$~', $currentHolding['l']);
+                $currentHolding['l'] = $l;
+                $currentHolding['~'] = ltrim(preg_replace('/^\d+/', ' ', $tilde));
+            }
+            
             if (count($currentHolding) > 0) {
                 $result[] = $currentHolding;
             }
         }
+        
+        
         return $result;
     }
 }
