136ebc9d9a52cdfab880b4c02700a8de6f093ff6
Jakub Sestak <sestak.jakub@gmail.com>
2015-03-16 19:35:53 +0100

    MZK Catalog: fixes in less

--- themes/mzk3/theme.config.php
+++ themes/mzk3/theme.config.php
@@ -1,9 +1,6 @@
 <?php
 return array(
     'extends' => 'common-bootstrap3',
-    'css' => array(
-        'mzk.css'
-    ),
     'favicon' => 'vufind-favicon.ico',
     'helpers' => array(
         'factories' => array(
