080ece7d90c23dd1125b6cb44e71df6f09f49a62
MartinKravec <kravec.martin@gmail.com>
2015-06-10 12:28:37 +0200

    License added

--- module/MZKCommon/src/MZKCommon/Statistics/PiwikStatistics.php
+++ module/MZKCommon/src/MZKCommon/Statistics/PiwikStatistics.php
@@ -1,4 +1,27 @@
 <?php
+/**
+ * PiwikStatistics Model Class
+ *
+ * PHP version 5
+ *
+ * Copyright (C) MZK 2015.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @author	Martin Kravec	<kravec@mzk.cz>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ */
 namespace MZKCommon\Statistics;
 
 use Piwik\API\Request;
@@ -9,6 +32,7 @@ use MZKCommon\Statistics\PiwikStatisticsInterface;
  * Calls Piwik's API and returns it's data
  * 
  * @author	Martin Kravec	<kravec@mzk.cz>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  */
 class PiwikStatistics implements PiwikStatisticsInterface
 {
