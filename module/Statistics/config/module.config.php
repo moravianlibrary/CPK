c8d914999b5912c63d754be4b6295e147c7ac908
Vaclav Rosecky <xrosecky@gmail.com>
2015-07-07 08:06:30 +0200

    Statistics module: useless declarations of routes removed

--- module/Statistics/config/module.config.php
+++ module/Statistics/config/module.config.php
@@ -61,46 +61,4 @@ $config = array(
     ),
 );
 
-$staticRoutes = array(
-    'Search/Conspectus', 'Search/MostSearched', 'Search/NewAcquisitions',
-    'MyResearch/CheckedOutHistory', 'MyResearch/ShortLoans',
-    'MyResearch/FavoritesImport', 'MyResearch/ProfileChange',
-	'Statistics/Dashboard', 'Statistics/Visits', 'Statistics/Circulations',
-	'Statistics/Payments', 'Statistics/Searches', 'Statistics', 'Statistics/',
-);
-
-foreach ($staticRoutes as $route) {
-    list($controller, $action) = explode('/', $route);
-    $routeName = str_replace('/', '-', strtolower($route));
-    $config['router']['routes'][$routeName] = array(
-        'type' => 'Zend\Mvc\Router\Http\Literal',
-        'options' => array(
-            'route'    => '/' . $route,
-            'defaults' => array(
-                'controller' => $controller,
-                'action'     => (! empty($action)) ? $action : 'default',
-            )
-        )
-    );
-}
-
-$nonTabRecordActions = array('ShortLoan');
-
-foreach ($nonTabRecordActions as $action) {
-    $config['router']['routes']['record' . '-' . strtolower($action)] = array(
-        'type'    => 'Zend\Mvc\Router\Http\Segment',
-        'options' => array(
-            'route'    => '/' . 'Record' . '/[:id]/' . $action,
-            'constraints' => array(
-                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
-            ),
-            'defaults' => array(
-                'controller' => 'Record',
-                'action'     => $action,
-            )
-        )
-    );
-}
-
 return $config;
