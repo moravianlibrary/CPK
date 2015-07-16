d988f2c4272aa51f6fa43adbbfe0ff65407b7e24
jirislav <mail@jkozlovsky.cz>
2015-07-16 14:05:41 +0200

    Created sh script for fetching all eduID connected IdP's DisplayNames to show in library cards menu Please note that the provided script should be used with caution & always be checked for printing institutions on one line!

---
+++ local/cpk-front.mzk.cz/languages/getAllEduIDInstitutes.sh
@@ -0,0 +1,3 @@
+#/bin/sh
+curl https://metadata.eduid.cz/entities/eduid+idp | grep -E "Scope|DisplayName xml:lang=\"cs\"" | cut -d ">" -f 2 | awk -F"<" '{print $1 }' | awk '!a[$0]++' | grep -vE "v.v.i.|v. v. i." | sed 's_\(.*[.]cz\)_scope\_\1 = _g' | sed 'N;s/=\(.*\)\n/= /g'
+
