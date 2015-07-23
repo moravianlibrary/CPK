8e239790d1af3344e901f33033515290c95e3e83
Jakub Šesták <sestak.jakub@gmail.com>
2015-07-23 15:03:28 +0200

    cpk: getting obalkyknih.cz comments via API

--- module/CPK/src/CPK/RecordDriver/SolrMarc.php
+++ module/CPK/src/CPK/RecordDriver/SolrMarc.php
@@ -41,23 +41,42 @@ class SolrMarc extends ParentSolrMarc
      */
     public function getObalkyKnihComments()
     {
-        $comment = new \stdClass();
-        $comment->firstname = 'Jmeno';
-        $comment->lastname = 'Prijmeni';
-        $comment->created = 'datum vytvoreni';
-        $comment->comment = ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at efficitur mi. Pellentesque aliquet dolor ac ligula consequat dignissim. Quisque efficitur tellus nec sapien vehicula, vel pellentesque arcu varius. Nunc lectus augue, tristique quis congue id, condimentum a risus. Vivamus pulvinar vehicula velit at tempus. Praesent eu diam non mauris laoreet scelerisque sed et massa. Aenean nibh sapien, hendrerit sed mollis varius, dapibus ut quam. Proin dapibus suscipit risus sed congue. Duis vel libero non augue viverra scelerisque vitae at neque.
+        $isbnArray = $this->getBibinfoForObalkyKnihV3();
 
-Sed ac malesuada ex. Praesent nec ipsum ut augue interdum ultrices sed ut ex. Phasellus hendrerit id dui et ullamcorper. Vestibulum non gravida dui. Curabitur semper sit amet eros et maximus. Nulla aliquet sodales nisl et posuere. Nam facilisis, nulla aliquet vestibulum dictum, magna ipsum lobortis sapien, eget accumsan magna metus at nunc. Nam nisl dui, interdum non ex in, placerat vulputate ligula. Suspendisse ipsum enim, posuere eu accumsan et, dignissim eget ipsum. Etiam fringilla velit quam. Nulla feugiat iaculis ornare. Phasellus luctus ultricies bibendum. ';
+        $isbnJson = json_encode($isbnArray);
 
-        return [
-            $comment,
-            $comment,
-            $comment,
-        ];
-        $table = $this->getDbTable('Comments');
-        return $table->getForResource(
-            $this->getUniqueId(), $this->getResourceSource()
-        );
+        $client = new \Zend\Http\Client('http://cache.obalkyknih.cz/api/books');
+        $client->setParameterGet(array(
+            'multi'  => '['.$isbnJson.']',
+        ));
+
+        $response = $client->send();
+
+        $responseBody = $response->getBody();
+
+
+        $phpResponse = json_decode($responseBody);
+
+
+        echo '<p>Hodnocení knihy:</p><img src="' . $phpResponse[0]->rating_url . '"/>';
+
+        $commentArray = array();
+
+        $i=0;
+
+        foreach ($phpResponse[0]->reviews as $review)
+        {
+            $com = new \stdClass();
+            $com->firstname = $review->library_name;
+            $com->created = $review->created;
+            $com->comment = $review->html_text;
+
+            $commentArray[$i] =  $com;
+            $i++;
+        }
+
+
+        return $commentArray;
     }
     
 }
