45debe5727107c0d79ac8c01a872665d780c57f5
Vaclav Rosecky <xrosecky@gmail.com>
2015-08-25 07:23:15 +0200

    short loans adopted to new time intervals

--- module/MZKCatalog/src/MZKCatalog/Controller/MyResearchController.php
+++ module/MZKCatalog/src/MZKCatalog/Controller/MyResearchController.php
@@ -79,7 +79,7 @@ class MyResearchController extends MyResearchControllerBase
             $view->profile = $profile;
             $expire = date_create_from_format('d. m. Y', $view->profile['expire']);
             $dateDiff = date_diff($expire, date_create());
-            if ($dateDiff->days < 30 && $dateDiff->invert != 0) {
+            if ($dateDiff->days < 31 && $dateDiff->invert != 0) {
                 $this->flashMessenger()->setNamespace('error')->addMessage('library_card_expiration_warning');
                 $view->prolongRegistrationUrl = $catalog->getProlongRegistrationUrl($patron);
             }
