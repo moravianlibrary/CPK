46be2ae23d61f0a7c16833832cb12cccfd430eb6
jirislav <mail@jkozlovsky.cz>
2015-11-12 21:13:52 +0100

    Notifications - returned the sqlUpdate for cpk-front.mzk.cz to initial state as we probably will not use the DB to determine the time user last queried notifications

--- local/cpk-front.mzk.cz/sqlUpdate.sql
+++ local/cpk-front.mzk.cz/sqlUpdate.sql
@@ -4,7 +4,3 @@ ALTER TABLE user CHANGE username username VARCHAR(64);
 /* Create indexed eppn for purposes of connected federative identities */
 ALTER TABLE user_card ADD COLUMN eppn VARCHAR(64);
 CREATE UNIQUE INDEX user_card_eppn_uq ON user_card(eppn);
-
-/* We will need user's last notificated time to don't spam all the ILSes every click */
-ALTER TABLE user ADD last_notificated DATETIME NULL DEFAULT '0000-00-00 00:00:00' AFTER created;
-CREATE INDEX last_notificated_dtime ON user(last_notificated);
