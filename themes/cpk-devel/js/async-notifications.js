df64f55db883b6a9260397ac7a5bd2f28b79b91a
jirislav <mail@jkozlovsky.cz>
2015-11-18 22:03:19 +0100

    Improved readability of notifications.js & precreated "addNotification" method

--- themes/cpk-devel/js/async-notifications.js
+++ themes/cpk-devel/js/async-notifications.js
@@ -23,7 +23,7 @@ var __notif = {
     getAllIdentityElements : function() {
 	return $('[data-identity].notification');
     },
-    
+
     addToCounter : function(count) {
 	if (count != 0) {
 
@@ -36,189 +36,14 @@ var __notif = {
 	    counter.show();
 	}
     },
-
-    blocks : {
-	// Time to wait until next refresh of the blocks in milisecs
-	toWait : 60 * 60 * 1000,
-	// We want only to save the fetched items after all institutions are
-	// fetched
-	institutionsToFetch : 0, // it'll get incremented as we'll iterate
-	// institutions
-	responses : {},
-	lastSaved : 0,
-	// Async notifications loader
-	fetchBlocks : function() {
-
-	    __notif.getAllIdentityElements().each(function() {
-		++__notif.blocks.institutionsToFetch;
-
-		var cat_username = $(this).attr('data-identity');
-
-		__notif.blocks.fetchBlocksForCatUsername(cat_username);
-	    });
-	},
-	// Create a query to fetch notifications about one institution
-	fetchBlocksForCatUsername : function(cat_username) {
-
-	    $.ajax({
-		type : 'POST',
-		url : '/AJAX/JSON?method=fetchBlocks',
-		dataType : 'json',
-		async : true,
-		// json object to sent to the authentication url
-		data : {
-		    cat_username : cat_username
-		},
-		success : function(response) {
-		    __notif.blocks.updateNotifies(response);
-		    __notif.blocks.processNotificationsFetched(response);
-		},
-		error : function(err) {
-		    __notif.printErr(err);
-		}
-	    });
-	},
-	// Recovers saved notifications from the local database
-	processSavedBlocks : function() {
-
-	    // Decide whether will we renew the notifications
-	    if (this.lastSaved) {
-		var shouldWeFetchAgain = this.toWait + this.lastSaved < Date
-			.now();
-
-		if (shouldWeFetchAgain)
-		    return this.fetchBlocks();
-	    } else {
-		return this.fetchBlocks();
-	    }
-
-	    // Check for another identities / delete disconnected ones
-	    this.syncIdentities();
-
-	    // Print saved values ..
-	    $.each(this.responses, function(i, response) {
-		__notif.blocks.processNotificationsFetched(response);
-	    });
-	},
-	// Updates saved notifications
-	updateNotifies : function(response) {
-
-	    var cat_username = response.data.cat_username;
-
-	    this.responses[cat_username] = response;
-
-	    // have we fetched all the institutions ?
-	    if (Object.keys(this.responses).length == this.institutionsToFetch) {
-
-		// This one is called only after fresh notifications were
-		// fetched
-		this.saveLastNotifies();
-	    }
-	},
-	// This function will render the html passed to it ..
-	processNotificationsFetched : function(response) {
-
-	    var data = response.data, status = response.status;
-
-	    var cat_username = data.cat_username, html = data.html, count = data.count;
-
-	    var jquerySelector = '[data-identity=' + cat_username
-		    + '].notification';
-
-	    var div = $(jquerySelector);
-
-	    if (!div.length) {
-		console.error("jQuery selector '" + jquerySelector
-			+ "' returned zero matches !");
-		return null;
-	    }
-
-	    if (status == 'OK') {
-		// Overwrite current div with the new one from renderer
-		// FIXME do not remove the data-identity because of possible
-		// need of appending another notifications !
-		div[0].outerHTML = html;
-
-		__notif.addToCounter(count);
-
-	    } else { // We have recieved an error
-		div.children('i').remove();
-
-		var message = data.message;
-		if (typeof message == "undefined")
-		    message = "Unknown error occured";
-
-		div.children('span.label').text(message).removeClass(
-			'label-primary').addClass('label-danger');
-	    }
-	},
-	// Check for (dis)connected identities
-	syncIdentities : function() {
-	    // Keys of responses are actually cat_usernames
-	    var tmpIdentities = Object.keys(this.responses);
-
-	    __notif.getAllIdentityElements().each(
-		    function() {
-			var cat_username = $(this).attr('data-identity');
-
-			var i = tmpIdentities.indexOf(cat_username.replace(
-				/\./, "\\."));
-
-			if (i > -1) {
-			    // We found this identity in the storage
-			    tmpIdentities.splice(i, 1);
-			} else {
-			    // New identity connected
-
-			    // Update the institutionsToFetch int as we may
-			    // need it on "invoked refresh"
-			    ++__notif.blocks.institutionsToFetch;
-
-			    // Fetch notificatios for new cat_username
-			    __notif.blocks.fetchBlocksForCatUsername(cat_username);
-			}
-		    });
-
-	    if (tmpIdentities.length > 0) {
-		// Some identities were disconnected
-
-		// Update the institutionsToFetch int as we may need it on
-		// "invoked
-		// refresh"
-		__notif.blocks.institutionsToFetch -= tmpIdentities.length;
-
-		// Remove those disconnected identites from storage
-		__notif.blocks.clearIdentities(tmpIdentities);
-	    }
-	},
-	// Clears provided identity's stored notification
-	// Useful e.g. while disconnecting an account ..
-	clearIdentities : function(cat_usernames) {
-	    var responsesTmp = {};
-
-	    Object.keys(this.responses).forEach(function(key) {
-		if (cat_usernames.indexOf(key) == -1)
-		    responsesTmp[key] = __notif.blocks.responses[key];
-	    });
-
-	    this.responses = responsesTmp;
-
-	    // This one is called only if we have young enough notifications
-	    this.saveLastNotifies();
-
-	},
-	// Do not call this function twice - as it'd probably result in an error
-	saveLastNotifies : function() {
-	    var lastNotifies = {
-		responses : this.responses,
-		timeSaved : Date.now()
-	    };
-
-	    localforage.setItem('notifies', lastNotifies, function(err, val) {
-		__notif.printErr(err, val);
-	    });
-	},
-
+    // What must be instanceof Element
+    addNotification : function(cat_username, element) {
+	if (typeof element == "object" && element instanceof Element) {
+	    // TODO: Show the notification
+	} else {
+	    var message = "While using __notif.addNotification(cat_username, element) element must be instanceof Element !";
+	    console.error(message);
+	}
     },
     // Error printing function
     printErr : function(err, val) {
@@ -228,5 +53,186 @@ var __notif = {
 		console.error("value having problem with is '" + val + "'");
 	    }
 	}
-    }
+    },
 }
+
+__notif.blocks = {
+    // Time to wait until next refresh of the blocks in milisecs
+    toWait : 60 * 60 * 1000,
+    // We want only to save the fetched items after all institutions are
+    // fetched
+    institutionsToFetch : 0, // it'll get incremented as we'll iterate
+    // institutions
+    responses : {},
+    lastSaved : 0,
+    // Async notifications loader
+    fetchBlocks : function() {
+
+	__notif.getAllIdentityElements().each(function() {
+	    ++__notif.blocks.institutionsToFetch;
+
+	    var cat_username = $(this).attr('data-identity');
+
+	    __notif.blocks.fetchBlocksForCatUsername(cat_username);
+	});
+    },
+    // Create a query to fetch notifications about one institution
+    fetchBlocksForCatUsername : function(cat_username) {
+
+	$.ajax({
+	    type : 'POST',
+	    url : '/AJAX/JSON?method=fetchBlocks',
+	    dataType : 'json',
+	    async : true,
+	    // json object to sent to the authentication url
+	    data : {
+		cat_username : cat_username
+	    },
+	    success : function(response) {
+		__notif.blocks.updateNotifies(response);
+		__notif.blocks.processNotificationsFetched(response);
+	    },
+	    error : function(err) {
+		__notif.printErr(err);
+	    }
+	});
+    },
+    // Recovers saved notifications from the local database
+    processSavedBlocks : function() {
+
+	// Decide whether will we renew the notifications
+	if (this.lastSaved) {
+	    var shouldWeFetchAgain = this.toWait + this.lastSaved < Date.now();
+
+	    if (shouldWeFetchAgain)
+		return this.fetchBlocks();
+	} else {
+	    return this.fetchBlocks();
+	}
+
+	// Check for another identities / delete disconnected ones
+	this.syncIdentities();
+
+	// Print saved values ..
+	$.each(this.responses, function(i, response) {
+	    __notif.blocks.processNotificationsFetched(response);
+	});
+    },
+    // Updates saved notifications
+    updateNotifies : function(response) {
+
+	var cat_username = response.data.cat_username;
+
+	this.responses[cat_username] = response;
+
+	// have we fetched all the institutions ?
+	if (Object.keys(this.responses).length == this.institutionsToFetch) {
+
+	    // This one is called only after fresh notifications were
+	    // fetched
+	    this.saveLastNotifies();
+	}
+    },
+    // This function will render the html passed to it ..
+    processNotificationsFetched : function(response) {
+
+	var data = response.data, status = response.status;
+
+	var cat_username = data.cat_username, html = data.html, count = data.count;
+
+	var jquerySelector = '[data-identity=' + cat_username
+		+ '].notification';
+
+	var div = $(jquerySelector);
+
+	if (!div.length) {
+	    console.error("jQuery selector '" + jquerySelector
+		    + "' returned zero matches !");
+	    return null;
+	}
+
+	if (status == 'OK') {
+	    // Overwrite current div with the new one from renderer
+	    // FIXME do not remove the data-identity because of possible
+	    // need of appending another notifications !
+	    div[0].outerHTML = html;
+
+	    __notif.addToCounter(count);
+
+	} else { // We have recieved an error
+	    div.children('i').remove();
+
+	    var message = data.message;
+	    if (typeof message == "undefined")
+		message = "Unknown error occured";
+
+	    div.children('span.label').text(message).removeClass(
+		    'label-primary').addClass('label-danger');
+	}
+    },
+    // Check for (dis)connected identities
+    syncIdentities : function() {
+	// Keys of responses are actually cat_usernames
+	var tmpIdentities = Object.keys(this.responses);
+
+	__notif.getAllIdentityElements().each(function() {
+	    var cat_username = $(this).attr('data-identity');
+
+	    var i = tmpIdentities.indexOf(cat_username.replace(/\./, "\\."));
+
+	    if (i > -1) {
+		// We found this identity in the storage
+		tmpIdentities.splice(i, 1);
+	    } else {
+		// New identity connected
+
+		// Update the institutionsToFetch int as we may
+		// need it on "invoked refresh"
+		++__notif.blocks.institutionsToFetch;
+
+		// Fetch notificatios for new cat_username
+		__notif.blocks.fetchBlocksForCatUsername(cat_username);
+	    }
+	});
+
+	if (tmpIdentities.length > 0) {
+	    // Some identities were disconnected
+
+	    // Update the institutionsToFetch int as we may need it on
+	    // "invoked
+	    // refresh"
+	    __notif.blocks.institutionsToFetch -= tmpIdentities.length;
+
+	    // Remove those disconnected identites from storage
+	    __notif.blocks.clearIdentities(tmpIdentities);
+	}
+    },
+    // Clears provided identity's stored notification
+    // Useful e.g. while disconnecting an account ..
+    clearIdentities : function(cat_usernames) {
+	var responsesTmp = {};
+
+	Object.keys(this.responses).forEach(function(key) {
+	    if (cat_usernames.indexOf(key) == -1)
+		responsesTmp[key] = __notif.blocks.responses[key];
+	});
+
+	this.responses = responsesTmp;
+
+	// This one is called only if we have young enough notifications
+	this.saveLastNotifies();
+
+    },
+    // Do not call this function twice - as it'd probably result in an error
+    saveLastNotifies : function() {
+	var lastNotifies = {
+	    responses : this.responses,
+	    timeSaved : Date.now()
+	};
+
+	localforage.setItem('notifies', lastNotifies, function(err, val) {
+	    __notif.printErr(err, val);
+	});
+    },
+
+};
