3a2749bd3f8f31f0cb5a944f8813af035ae0ad8e
MartinKravec <kravec.martin@gmail.com>
2015-12-15 12:56:11 +0100

    Added and filled citation styles table

--- local/cpk-front.mzk.cz/sqlUpdate.sql
+++ local/cpk-front.mzk.cz/sqlUpdate.sql
@@ -7,3 +7,30 @@ CREATE UNIQUE INDEX user_card_eppn_uq ON user_card(eppn);
 
 /* Create row for storing user's read notifications */
 ALTER TABLE user ADD read_notifications VARCHAR(512) NULL DEFAULT NULL;
+
+/* Create and fill table representing citation styles */
+CREATE TABLE IF NOT EXISTS `citation_style` (
+  `id` int(11) NOT NULL,
+  `description` VARCHAR(32),
+  `value` VARCHAR(8)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+ALTER TABLE `citation_style`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `citation_style`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+
+INSERT INTO `vufind`.`citation_style` (`id`, `description`, `value`) 
+  VALUES (NULL, 'ČSN ISO 690', '38673'),
+         (NULL, 'Harvard', '3'), 
+         (NULL, 'NISO/ANSI Z39.29 (2005)', '4'),
+         (NULL, 'MLA (7th edition)', '5'), 
+         (NULL, 'Turabian (7th edition)', '6'), 
+         (NULL, 'Chicago (16th edition)', '7'), 
+         (NULL, 'IEEE', '8'), 
+         (NULL, 'CSE', '9'), 
+         (NULL, 'CSE NY', '10'), 
+         (NULL, 'APA', '11'), 
+         (NULL, 'ISO 690', '12');
+
