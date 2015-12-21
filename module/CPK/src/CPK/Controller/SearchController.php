af229e5d9e79a85e9a327858e684e6e298de27ed
MartinKravec <kravec.martin@gmail.com>
2015-12-21 09:48:38 +0100

    Applied new user settings to advanced search

--- module/CPK/src/CPK/Controller/SearchController.php
+++ module/CPK/src/CPK/Controller/SearchController.php
@@ -4,7 +4,7 @@
  *
  * PHP version 5
  *
- * Copyright (C) MZK 2015.
+ * Copyright (C) Moravian Library 2015.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
@@ -19,8 +19,8 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * @author	Martin Kravec	<kravec@mzk.cz>
- * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @author  Martin Kravec <martin.kravec@mzk.cz>
+ * @license http://opensource.org/licenses/gpl-3.0.php GNU General Public License
  */
 namespace CPK\Controller;
 
@@ -29,8 +29,8 @@ use VuFind\Controller\SearchController as SearchControllerBase;
 /**
  * SearchController
  *
- * @author	Martin Kravec	<kravec@mzk.cz>
- * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @author  Martin Kravec <martin.kravec@mzk.cz>
+ * @license http://opensource.org/licenses/gpl-3.0.php GNU General Public License
  */
 class SearchController extends SearchControllerBase
 {
@@ -63,6 +63,65 @@ class SearchController extends SearchControllerBase
 		$view->institutionsMappings = $institutionsMappings;
 
 		return $view;
+	}
+	
+	/**
+	 * Handle an advanced search
+	 *
+	 * @return mixed
+	 */
+	public function advancedAction()
+	{
+	    // Standard setup from base class:
+	    $view = parent::advancedAction();
+	
+	    // Set up facet information:
+	    $view->facetList = $this->processAdvancedFacets(
+	        $this->getAdvancedFacets()->getFacetList(), $view->saved
+	        );
+	
+	    $prefferedFacets = array();
+	    $config = $this->getServiceLocator()->get('VuFind\Config')->get('facets');
+	
+	    if (count($config->PreferredFacets)) {
+	        foreach ($config->PreferredFacets as $field => $values) {
+	            $vals = array();
+	            $i = 0;
+	            foreach ($values as $val) {
+	                $i++;
+	                $vals[$val] = $i;
+	            }
+	            $prefferedFacets[$field] = $vals;
+	        }
+	    }
+	
+	    $view->preferredFacets = $prefferedFacets;
+	
+	    $specialFacets = $this->parseSpecialFacetsSetting(
+	        $view->options->getSpecialAdvancedFacets()
+	        );
+	    if (isset($specialFacets['illustrated'])) {
+	        $view->illustratedLimit
+	        = $this->getIllustrationSettings($view->saved);
+	    }
+	    if (isset($specialFacets['checkboxes'])) {
+	        $view->checkboxFacets = $this->processAdvancedCheckboxes(
+	            $specialFacets['checkboxes'], $view->saved
+	            );
+	    }
+	    $view->ranges = $this->getAllRangeSettings($specialFacets, $view->saved);
+	    $view->hierarchicalFacets = $this->getHierarchicalFacets();
+	    
+	    // Has user preferred user settings?
+	    if ($user = $this->getAuthManager()->isLoggedIn()) {
+	        $userSettingsTable = $this->getTable("usersettings");
+	    
+	        $preferredRecordsPerPage = $userSettingsTable->getRecordsPerPage($user);
+	        if (! empty($preferredRecordsPerPage))
+	            $view->preferredRecordsPerPage = $preferredRecordsPerPage;
+	    }
+	
+	    return $view;
 	}
 
     /**
