a26ef21826ce1635fd36b4dab9562ddaec0c2bd1
Martin Kravec <kravec.martin@gmail.com>
2016-01-25 08:50:49 +0100

    Added non responsive records in libraries

--- themes/cpk-devel/js/search-results.js
+++
@@ -1,7 +0,0 @@
-function showNextInstitutions(obj) {
-    var anchors = obj.parentNode.parentNode.getElementsByTagName('a');
-    
-    $(anchors).each(function(key, val) {val.removeAttribute('hidden')});
-    
-    obj.remove();
-}
