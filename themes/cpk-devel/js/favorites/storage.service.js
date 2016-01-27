1545ea1ac4358f8342abe1c8e6ba0f724c5a6249
jirislav <mail@jkozlovsky.cz>
2016-01-27 14:21:31 +0100

    favs: finished storage service

--- themes/cpk-devel/js/favorites/storage.service.js
+++ themes/cpk-devel/js/favorites/storage.service.js
@@ -18,6 +18,15 @@
 	    name : '_fav'
 	};
 
+	var favorites = [];
+	
+	var initialize = {
+		done : false,
+		buffer : []
+	};
+
+	initialize();
+
 	return storage;
 
 	// Public methods
@@ -26,51 +35,115 @@
 	    return new Promise(function(resolve, reject) {
 
 		if (favorite instanceof Favorite) {
-
-		    resolve();
+		    
+		    saveFavorite(favorite).then(resolve).catch(reject);
+		    
 		} else {
-		    $log.error('storage.addFavorite(Favorite favorite) - instanceof Favorite must be provided !');
-		    reject();
+		    reject('storage.addFavorite(Favorite favorite) - instanceof Favorite must be provided !');
 		}
 	    });
 	}
 
 	function deleteFavorite(id) {
-	    $log('Favorite with id ' + id + ' removed !');
+	    return new Promise(function(resolve, reject) {
+		
+		if (typeof id === "number" && id >= 0) {
+		    
+		    if (typeof favorites[id] === "undefined") {
+			reject('Favorite does not exist with this ID');
+		    } else {
+			
+			// Remove that Favorite
+			favorites.splice(id, 1);
+			
+			// Save those Favorites
+			saveFavorites().then(resolve).catch(reject);
+		    };
+		} else {
+		    reject('Wrong id provided to delete an Favorite from sessionStorage');
+		}
+	    });
 	}
+	
+	function getFavorite(id) {
+	    return new Promise(function(resolve, reject) {
+		
+		if (typeof id === "number" && id >= 0) {
+		    
+		    var theJob = function() {
+			resolve(favorite(id));
+		    };
 
-	function getFavorite() {
-
+		    executeAfterInitialized(theJob);
+		    
+		} else {
+		    reject('Cannoct get favorite not having an index');
+		};
+	    });
 	}
 
 	function getFavorites() {
-
+	    return new Promise(function(resolve, reject) {
+		
+		var theJob = function() {
+		    resolve(favorites);
+		};
+		
+		executeAfterInitialized(theJob);
+	    });
 	}
 
 	// Private methods
 
-	function saveObject(obj) {
-	    if (typeof obj === "object") {
-		localforage.setDriver(localforage.SESSIONSTORAGE);
-
-		localforage.getItem(storage.name).then(save(obj)); // ?? ... TODO
-
-		localforage.setLastDriver(); // See
-						// js/vendor/localforage-customizations.js
-
-		function save(obj) {
-		    return function(favorites) {
-			if (typeof favorites === "object") {
-			    if (typeof favorites.totalCount === "number") {
-				++favorites.totalCount;
-			    } else {
-				favorites.totalCount = 1;
-			    }
-			}
-		    }
+	function saveFavorite(favorite) {
+	    return new Promise(function(resolve, reject) {
+		
+		    favorites.push(favorite);
+		    
+		    saveFavorites().then(resolve).catch(reject);
+	    });
+	}
+	
+	function saveFavorites() {
+	    return new Promise(function(resolve, reject) {
+		
+		var theJob = function() {
+		    localforage.setItem(storage.name, favorites).then(resolve).catch(reject);
+		};
+		
+		executeAfterInitialized(theJob);
+	    });
+	}
+	
+	function executeAfterInitialized(func) {
+	    if (typeof func === "function")
+		if (initialize.done) {
+		    func.call();
+		
+		} else {
+		    // Execute after an initialization
+	    	    initialize.buffer.push(func);
 		}
-	    }
 	}
 
-    }
+	function initialize() {
+
+	    localforage.getSessionItem(storage.name).then(function(val) {
+		
+		if (val)
+		    favorites = val;
+		
+		initialize.done = true;
+		
+		initialize.buffer.forEach(function(func) {
+		    func.call();
+		});
+		
+	    }).catch(function(reason) {
+		
+		$log.error(reason);
+	    });
+	};
+
+    };
 })();
