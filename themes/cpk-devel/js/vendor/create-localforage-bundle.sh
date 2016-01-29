b2e541df15f7fb5a8bab247d5b4d64a90b8f83c7
jirislav <mail@jkozlovsky.cz>
2016-01-29 17:49:12 +0100

    localforage: Minimized customizations I've basically removed the sessionStorage wrapper as it was increasing the app complexity rapidly .. the AngularJS app for the "offline" favorites now has it's own sessionStorage implementation

--- themes/cpk-devel/js/vendor/create-localforage-bundle.sh
+++ themes/cpk-devel/js/vendor/create-localforage-bundle.sh
@@ -12,9 +12,17 @@ localforage_url="https://github.com/mozilla/localForage/raw/master/dist/localfor
 
 echo "Downloading latest localforage.min.js .."
 curl -Ls "$localforage_url" > "$dest"
-localforage_sessionstoragewrapper="https://github.com/thgreasi/localForage-sessionStorageWrapper/raw/master/src/localforage-sessionstoragewrapper.js"
+
+#localforage_sessionstoragewrapper="https://github.com/thgreasi/localForage-sessionStorageWrapper/raw/master/src/localforage-sessionstoragewrapper.js"
 localforage_customizations="localforage-customizations.js"
 
 echo "Requesting google closure compiler to concatenate & compress & compile latest sessionstorage driver & our customizations together .."
-curl -Ls --data-urlencode "code_url=${localforage_sessionstoragewrapper}" --data-urlencode js_code@${localforage_customizations} -d output_info=compiled_code https://closure-compiler.appspot.com/compile >> "$dest"
-echo "All done!"
+#curl -Ls --data-urlencode "code_url=${localforage_sessionstoragewrapper}" --data-urlencode js_code@${localforage_customizations} -d output_info=compiled_code https://closure-compiler.appspot.com/compile >> "$dest"
+curl -Ls --data-urlencode js_code@${localforage_customizations} -d output_info=compiled_code https://closure-compiler.appspot.com/compile >> "$dest"
+
+if [ "$(grep "ERROR:" "$dest")" ]; then
+	echo " ERROR !"
+	echo "An error occured ! Please see the file \"$dest\""
+else
+	echo "All done!"
+fi
