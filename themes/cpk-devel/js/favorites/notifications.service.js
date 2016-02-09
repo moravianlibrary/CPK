b7f7743f8798d260e2300f59b468f23846876fa0
jirislav <mail@jkozlovsky.cz>
2016-02-09 20:08:18 +0100

    favs: Added notifications enabled check

--- themes/cpk-devel/js/favorites/notifications.service.js
+++ themes/cpk-devel/js/favorites/notifications.service.js
@@ -8,40 +8,47 @@
 
     function notificationsService(translate) {
 
+	// Public object
 	var notifications = {
 	    favAdded : favAdded,
 	    allFavsRemoved : allFavsRemoved
 	};
 
+	// Private vars
 	var addedSomethingAlready = false;
+	var notificationsEnabled = typeof __notif !== 'undefined';
 
 	return notifications;
 
 	// Public methods
 
 	function favAdded() {
+	    if (notificationsEnabled === true) {
+		
+		if (addedSomethingAlready === false) {
+		    addedSomethingAlready = true;
 
-	    if (addedSomethingAlready === false) {
-		addedSomethingAlready = true;
-
-		createNotificationWarning();
+		    createNotificationWarning();
+		}
 	    }
 	}
 
 	function allFavsRemoved() {
+	    if (notificationsEnabled === true) {
 
-	    // Remove the notification
-	    __notif.helper.pointers.global.children('.notif-favs').remove();
+		// Remove the notification
+		__notif.helper.pointers.global.children('.notif-favs').remove();
 
-	    // Remove the warning icon if there is no more notifs
-	    if (__notif.sourcesRead.unreadCount === 0) {
+		// Remove the warning icon if there is no more notifs
+		if (__notif.sourcesRead.unreadCount === 0) {
 
-		addedSomethingAlready = false;
+		    addedSomethingAlready = false;
 
-		// Hide warning icon ..
-		__notif.warning.hide();
+		    // Hide warning icon ..
+		    __notif.warning.hide();
 
-		__notif.helper.pointers.global.children().first().show();
+		    __notif.helper.pointers.global.children().first().show();
+		}
 	    }
 	}
 
