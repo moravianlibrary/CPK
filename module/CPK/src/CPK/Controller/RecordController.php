bf876b7aeb3b6da9e386e1d40eb438e187bc184f
Martin Kravec <kravec.martin@gmail.com>
2016-02-23 07:30:22 +0100

    Added redirection when problem with sending email

--- module/CPK/src/CPK/Controller/RecordController.php
+++ module/CPK/src/CPK/Controller/RecordController.php
@@ -287,51 +287,48 @@ xsi:schemaLocation="http://www.openarchives.org/OAI/2.0/
         $config = $this->getConfig();
         if ((!isset($config->Mail->require_login) || $config->Mail->require_login)
             && !$this->getUser()
-            ) {
-                return $this->forceLogin();
-            }
+        ) {
+            return $this->forceLogin();
+        }
     
-            // Retrieve the record driver:
-            $driver = $this->loadRecord();
-    
-            // Create view
-            $mailer = $this->getServiceLocator()->get('VuFind\Mailer');
-            $view = $this->createEmailViewModel(
-                null, $mailer->getDefaultRecordSubject($driver)
+        // Retrieve the record driver:
+        $driver = $this->loadRecord();
+
+        // Create view
+        $mailer = $this->getServiceLocator()->get('VuFind\Mailer');
+        $view = $this->createEmailViewModel(
+            null, $mailer->getDefaultRecordSubject($driver)
+            );
+        $mailer->setMaxRecipients($view->maxRecipients);
+
+        // Set up reCaptcha
+        $view->useRecaptcha = $this->recaptcha()->active('email');
+        // Process form submission:
+        if ($this->formWasSubmitted('submit', $view->useRecaptcha)) {
+            // Attempt to send the email and show an appropriate flash message:
+            try {
+                $subject = $driver->getTitle();
+                $cc = $this->params()->fromPost('ccself') && $view->from != $view->to
+                ? $view->from : null;
+                $sender = new \Zend\Mail\Address(
+                    $view->from, 
+                    $this->translate('Central Library Portal')
                 );
-            $mailer->setMaxRecipients($view->maxRecipients);
-    
-            // Set up reCaptcha
-            $view->useRecaptcha = $this->recaptcha()->active('email');
-            // Process form submission:
-            if ($this->formWasSubmitted('submit', $view->useRecaptcha)) {
-                // Attempt to send the email and show an appropriate flash message:
-                try {
-                    $subject = $driver->getTitle();
-                    $cc = $this->params()->fromPost('ccself') && $view->from != $view->to
-                    ? $view->from : null;
-                    $sender = new \Zend\Mail\Address(
-                        $view->from, 
-                        $this->translate('Central Library Portal')
-                    );
-                    $mailer->sendRecord(
-                        $view->to, 
-                        $sender, 
-                        $view->message, 
-                        $driver,
-                        $this->getViewRenderer(),
-                        $subject,
-                        $cc
-                    );
-                    $this->flashMessenger()->addMessage('email_success', 'success');
-                    return $this->redirectToRecord();
-                } catch (MailException $e) {
-                    $this->flashMessenger()->addMessage($e->getMessage(), 'error');
-                }
+                $mailer->sendRecord(
+                    $view->to, 
+                    $sender, 
+                    $view->message, 
+                    $driver,
+                    $this->getViewRenderer(),
+                    $subject,
+                    $cc
+                );
+                $this->flashMessenger()->addMessage('email_success', 'success');
+                return $this->redirectToRecord();
+            } catch (MailException $e) {
+                $this->flashMessenger()->addMessage($e->getMessage(), 'error');
             }
-    
-            // Display the template:
-            $view->setTemplate('record/email');
-            return $view;
+        }
+        return $this->redirectToRecord();
     }
 }
