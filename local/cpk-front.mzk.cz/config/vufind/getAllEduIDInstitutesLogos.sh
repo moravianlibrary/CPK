125815ffd62f963b6eec0d76788fd015f9a5a057
jirislav <mail@jkozlovsky.cz>
2016-03-24 11:57:50 +0100

    Purged unused shell script for fetching logos

--- local/cpk-front.mzk.cz/config/vufind/getAllEduIDInstitutesLogos.sh
+++
@@ -1,3 +0,0 @@
-#/bin/sh
-curl -s https://metadata.eduid.cz/entities/eduid+idp | grep -E "Logo |Scope" | cut -d">" -f2 | cut -d"<" -f1 | sed -r "s_\s+__g" | sed "N;s_\([.]cz\)\([\n\s]*\)http_\1 = http_g" | sed "N;s_\([.]cz\)\([\n\s]*\)http_\1 = http_g" | sed "N;s_\([.]cz\)\([\n\s]*\)http_\1 = http_g" | grep "="
-
