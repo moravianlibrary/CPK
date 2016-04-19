6b220ebbd04e08b39ea504e51eea9a10c68dcf15
jirislav <mail@jkozlovsky.cz>
2016-04-19 22:31:35 +0200

    Removed unused shell script

--- local/cpk-front.mzk.cz/languages/getAllEduIDInstitutes.sh
+++
@@ -1,2 +0,0 @@
-#/bin/sh
-curl -s https://metadata.eduid.cz/entities/eduid+idp | grep -E "Scope|DisplayName xml:lang=\"cs\"" | cut -d ">" -f 2 | awk -F"<" '{print $1 }' | awk '!a[$0]++' | grep -vE "v.v.i.|v. v. i." | sed 's_\(.*[.]cz\)_source\_\1 = _g' | sed 'N;s/=\(.*\)\n/= /g'
