ed99211eac5d5ae06a28728e38e4a03db41f1cea
Martin Kravec <kravec.martin@gmail.com>
2016-07-26 09:17:41 +0200

    Autocomplete now support diacritic and is case insensitive

--- module/CPK/src/CPK/Autocomplete/SolrEdgeFaceted.php
+++ module/CPK/src/CPK/Autocomplete/SolrEdgeFaceted.php
@@ -62,7 +62,7 @@ class SolrEdgeFaceted extends ParentSolrEdgeFaceted
         $results = array();
         try {
             $this->searchObject->getParams()->setBasicSearch(
-                $this->mungeQuery($query), $this->facetField
+                $this->autocompleteField.':('.$this->mungeQuery($query).')'
             );
             $params = $this->searchObject->getParams();
             $options = $this->searchObject->getOptions();
@@ -77,13 +77,14 @@ class SolrEdgeFaceted extends ParentSolrEdgeFaceted
             }
             $params->addFacet($this->facetField);
             $params->setLimit(0);
-            $params->setFacetLimit(25);
+            $params->setFacetLimit(30);
             $this->searchObject->getParams()->setSort($this->facetField);
             $results = $this->searchObject->getResults();
             $facets = $this->searchObject->getFacetList();
             if (isset($facets[$this->facetField]['list'])) {
+                $queryWithoutDiacritic = $this->removeDiacritic($query);
                 foreach ($facets[$this->facetField]['list'] as $filter) {
-                    if (stripos($filter['value'], $query) !== false) {
+                    if (stripos($this->removeDiacritic($filter['value']), $queryWithoutDiacritic) !== false) {
                         array_push($results, $filter['value']);
                     }
                 }
@@ -142,5 +143,96 @@ class SolrEdgeFaceted extends ParentSolrEdgeFaceted
             // Ignore errors -- just return empty results if we must.
         }
         return array_unique($results);
+    }
+
+    private function removeDiacritic($str) {
+        $conversionTable = [
+            'ä'=>'a',
+            'Ä'=>'A',
+            'á'=>'a',
+            'Á'=>'A',
+            'à'=>'a',
+            'À'=>'A',
+            'ã'=>'a',
+            'Ã'=>'A',
+            'â'=>'a',
+            'Â'=>'A',
+            'č'=>'c',
+            'Č'=>'C',
+            'ć'=>'c',
+            'Ć'=>'C',
+            'ď'=>'d',
+            'Ď'=>'D',
+            'ě'=>'e',
+            'Ě'=>'E',
+            'é'=>'e',
+            'É'=>'E',
+            'ë'=>'e',
+            'Ë'=>'E',
+            'è'=>'e',
+            'È'=>'E',
+            'ê'=>'e',
+            'Ê'=>'E',
+            'í'=>'i',
+            'Í'=>'I',
+            'ï'=>'i',
+            'Ï'=>'I',
+            'ì'=>'i',
+            'Ì'=>'I',
+            'î'=>'i',
+            'Î'=>'I',
+            'ľ'=>'l',
+            'Ľ'=>'L',
+            'ĺ'=>'l',
+            'Ĺ'=>'L',
+            'ń'=>'n',
+            'Ń'=>'N',
+            'ň'=>'n',
+            'Ň'=>'N',
+            'ñ'=>'n',
+            'Ñ'=>'N',
+            'ó'=>'o',
+            'Ó'=>'O',
+            'ö'=>'o',
+            'Ö'=>'O',
+            'ô'=>'o',
+            'Ô'=>'O',
+            'ò'=>'o',
+            'Ò'=>'O',
+            'õ'=>'o',
+            'Õ'=>'O',
+            'ő'=>'o',
+            'Ő'=>'O',
+            'ř'=>'r',
+            'Ř'=>'R',
+            'ŕ'=>'r',
+            'Ŕ'=>'R',
+            'š'=>'s',
+            'Š'=>'S',
+            'ś'=>'s',
+            'Ś'=>'S',
+            'ť'=>'t',
+            'Ť'=>'T',
+            'ú'=>'u',
+            'Ú'=>'U',
+            'ů'=>'u',
+            'Ů'=>'U',
+            'ü'=>'u',
+            'Ü'=>'U',
+            'ù'=>'u',
+            'Ù'=>'U',
+            'ũ'=>'u',
+            'Ũ'=>'U',
+            'û'=>'u',
+            'Û'=>'U',
+            'ý'=>'y',
+            'Ý'=>'Y',
+            'ž'=>'z',
+            'Ž'=>'Z',
+            'ź'=>'z',
+            'Ź'=>'Z'
+       ];
+
+       return strtr($str, $conversionTable);
     }
 }
