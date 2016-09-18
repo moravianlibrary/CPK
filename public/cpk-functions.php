a8cfdd133af9ae0f45b9a36d66daf51af9e23219
Martin Kravec <kravec.martin@gmail.com>
2016-09-18 15:02:07 +0200

    Prepared function to log fatal errors out of docker container

--- public/cpk-functions.php
+++ public/cpk-functions.php
@@ -8,7 +8,7 @@ function notEmpty($arr, $key)
 defined('USES_IE') || define('USES_IE', preg_match('/Windows/', $_SERVER['HTTP_USER_AGENT']));
 
 function standardErrorHandler($errno, $errstr, $errfile, $errline) {
-    echo "<b>Custom error:</b> [$errno] $errstr<br>";
+    echo "<b>Error:</b> [$errno] $errstr<br>";
     echo " Error on line $errline in $errfile<br>";
 }
 
@@ -21,7 +21,22 @@ function fatalErrorHandler() {
         ($error['type'] === E_USER_NOTICE)
     ) {
         include_once(__DIR__."/../themes/cpk-devel/templates/error/fatal-error.phtml");
+
+        /* @TODO: log this error out of the container */
+        //logFatalError($error);
     }
+}
+
+function logFatalError($error) {
+    $logFile = __DIR__."/../fatal-errors.log";
+    $fp = fopen($logFile, "a");
+    $logDetails = "ERROR : " . $error['type']
+    . " |Msg : " . $error['message']
+    . " |File : " . $error['file']
+    . " |Line : " . $error['line'];
+    fwrite($fp, $logDetails);
+    fwrite($fp, "");
+    fclose($fp);
 }
 
 set_error_handler("standardErrorHandler");
