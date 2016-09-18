78a7039c4c30efff31fb2f262c0600ca98512f88
Martin Kravec <kravec.martin@gmail.com>
2016-09-18 14:18:49 +0200

    Added fatal-error page and handler

--- public/cpk-functions.php
+++ public/cpk-functions.php
@@ -6,3 +6,16 @@ function notEmpty($arr, $key)
 }
 
 defined('USES_IE') || define('USES_IE', preg_match('/Windows/', $_SERVER['HTTP_USER_AGENT']));
+
+function standardErrorHandler($errno, $errstr, $errfile, $errline) {
+    echo "<b>Custom error:</b> [$errno] $errstr<br>";
+    echo " Error on line $errline in $errfile<br>";
+}
+
+
+function fatalErrorHandler() {
+    include(__DIR__."/../themes/cpk-devel/templates/error/fatal-error.phtml");
+}
+
+set_error_handler("standardErrorHandler");
+register_shutdown_function('fatalErrorHandler');
