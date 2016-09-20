a9163279b1feadacb75dd51dd1fdab8216182e18
Martin Kravec <kravec.martin@gmail.com>
2016-09-20 09:02:55 +0200

    Fixed undefined HTTP_USER_AGENT

--- public/cpk-functions.php
+++ public/cpk-functions.php
@@ -5,7 +5,16 @@ function notEmpty($arr, $key)
     return isset($arr[$key]) && ! empty($arr[$key]);
 }
 
-defined('USES_IE') || define('USES_IE', preg_match('/MSIE|Trident/', $_SERVER['HTTP_USER_AGENT']));
+/*
+ * The User-Agent header is optional.
+ * Firewalls may filter it or people may configure their clients to omit it.
+ * Simply check using isset() if it exists. Or even better, use !empty()
+ * as an empty header won't be useful either
+ */
+$userAgent = isset($_SERVER['HTTP_USER_AGENT'])
+    ? strtolower($_SERVER['HTTP_USER_AGENT'])
+    : '';
+defined('USES_IE') || define('USES_IE', preg_match('/MSIE|Trident/', $userAgent));
 
 function standardErrorHandler($errno, $errstr, $errfile, $errline) {
     echo "<b>Error:</b> [$errno] $errstr<br>";
