81ebc0232b2eb6559b9d86301e999ea840909080
MartinKravec <kravec.martin@gmail.com>
2017-05-12 15:51:57 +0200

    Send EDS records via email

--- module/CPK/src/CPK/Controller/RecordController.php
+++ module/CPK/src/CPK/Controller/RecordController.php
@@ -359,7 +359,12 @@ xsi:schemaLocation="http://www.openarchives.org/OAI/2.0/
         }
 
         // Retrieve the record driver:
-        $driver = $this->loadRecord();
+        $database = $this->params()->fromPost('source');
+        if ($database == 'EDS') {
+            $driver = $this->loadEdsRecord();
+        } else {
+            $driver = $this->loadRecord();
+        }
 
         // Create view
         $mailer = $this->getServiceLocator()->get('VuFind\Mailer');
@@ -397,6 +402,34 @@ xsi:schemaLocation="http://www.openarchives.org/OAI/2.0/
             }
         }
         return $this->redirectToRecord();
+    }
+
+    /**
+     * Load the record requested by the user; note that this is not done in the
+     * init() method since we don't want to perform an expensive search twice
+     * when homeAction() forwards to another method.
+     *
+     * @return AbstractRecordDriver
+     */
+    protected function loadEdsRecord()
+    {
+        // Only load the record if it has not already been loaded.  Note that
+        // when determining record ID, we check both the route match (the most
+        // common scenario) and the GET parameters (a fallback used by some
+        // legacy routes).
+        if (!is_object($this->driver)) {
+            $recordLoader = $this->getRecordLoader();
+            $cacheContext = $this->getRequest()->getQuery()->get('cacheContext');
+            if (isset($cacheContext)) {
+                $recordLoader->setCacheContext($cacheContext);
+            }
+            $this->driver = $recordLoader->load(
+                $this->params()->fromRoute('id', $this->params()->fromQuery('id')),
+                'EDS',
+                false
+            );
+        }
+        return $this->driver;
     }
 
     /**
