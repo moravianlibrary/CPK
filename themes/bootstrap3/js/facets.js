f440a255faa040704b5b0071d39fac648959ea02
Zdeněk Nováček <novacek@mzk.cz>
2017-12-07 11:51:03 +0100

    in before commit used wrong class

--- themes/bootstrap3/js/facets.js
+++ themes/bootstrap3/js/facets.js
@@ -470,7 +470,7 @@ jQuery( document ).ready( function( $ ) {
                 distance: 3,
                 direction: 'right'
             }, 200);
-            if ($(".alert-info").length === 0) {
+            if ($(".bootstrap-growl").length === 0) {
                 var isMobile = false; //initiate as false
                 // device detection
                 if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
