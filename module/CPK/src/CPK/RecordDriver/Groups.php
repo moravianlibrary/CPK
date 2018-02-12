040e0d89c62515173c1cde08f47b90c05dabe903
Zdeněk Nováček <znovacek1997@gmail.com>
2018-02-12 14:55:49 +0100

    rewrite records-in-groups and create class Groups

---
+++ module/CPK/src/CPK/RecordDriver/Groups.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: novacek
+ * Date: 12.2.18
+ * Time: 14:47
+ */
+
+namespace CPK\RecordDriver;
+
+
+class Groups
+{
+    public function getSortRecords($dedupedRecords) {
+
+        $tempCnbs = [];
+        $tempCaslins = [];
+        foreach ($dedupedRecords as $key => $record) {
+            if ($record['source'] === 'source_cnb') {
+                $tempCnbs[] = $record;
+                unset($dedupedRecords[$key]);
+            }
+            if ($record['source'] === 'source_caslin') {
+                $tempCaslins[] = $record;
+                unset($dedupedRecords[$key]);
+            }
+        }
+        sort($dedupedRecords);
+        foreach ($tempCnbs as $key => $record) {
+            $dedupedRecords[] = $record;
+        }
+        foreach ($tempCaslins as $key => $record) {
+            $dedupedRecords[] = $record;
+        }
+
+        return $dedupedRecords;
+    }
+
+    public function getInternalMultiplicity($dedupedRecords, $institution){
+        $Records = array();
+        foreach ($dedupedRecords as $key => $record) {
+            if (strpos('.'.$institution,substr($record['source'], 7)) && $institution !== $record['id']) {
+                array_push($Records, $record['id']);
+            }
+        }
+        return $Records;
+    }
+}
