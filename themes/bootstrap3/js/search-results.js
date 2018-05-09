ba8ac4af7117d6fae41eda78c1d4a87c495802d8
Andrew <blayson4@gmail.com>
2018-05-09 14:00:41 +0200

    Add additional question marks for rss and advanced search

--- themes/bootstrap3/js/search-results.js
+++ themes/bootstrap3/js/search-results.js
@@ -633,9 +633,13 @@ jQuery( document ).ready( function( $ ) {
                         if (data['database'] == 'Solr') {
                             $("#ci-autocomplete-help").attr('class', 'hidden')
                             $("#autocomplete-help").removeAttr('class')
+                            $("#ci-search-results-rss-help").attr('class', 'hidden')
+                            $("#search-results-rss-help").removeAttr('class')
                         } else if (data['database'] == "EDS") {
                             $("#autocomplete-help").attr('class', 'hidden');
                             $("#ci-autocomplete-help").removeAttr('class');
+                            $("#search-results-rss-help").attr('class', 'hidden')
+                            $("#ci-search-results-rss-help").removeAttr('class')
                         }
                     },
                     error: function (xmlHttpRequest, status, error) {
