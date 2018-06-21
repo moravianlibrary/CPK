c5238ff6b842d66c63c2af20bc8fa34d7489cf03
Andrew <blayson4@gmail.com>
2018-06-21 20:36:03 +0200

    Add ziskej controller and ziskej page

---
+++ module/CPK/src/CPK/Controller/ZiskejController.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Ziskej Controller
+ *
+ * PHP version 7
+ *
+ * Copyright (C) Moravian Library 2018.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @author  Andrii But <but@mzk.cz>
+ * @license http://opensource.org/licenses/gpl-3.0.php GNU General Public License
+ */
+
+namespace CPK\Controller;
+
+use VuFind\Controller\AbstractBase;
+
+
+class ZiskejController extends AbstractBase
+{
+    /**
+     * Home Action
+     *
+     * @return \Zend\View\Model\ViewModel
+     */
+    public function homeAction()
+    {
+        $view = $this->createViewModel();
+        $view->setTemplate('ziskej/ziskej');
+
+        // Check if it is post request
+        if ($this->getRequest()->isPost()) {
+            $data = $this->getRequest()->getPost('ziskej');
+
+            // Set cookie
+            setcookie('ziskej', htmlspecialchars($data));
+        }
+
+        return $view;
+    }
+}
