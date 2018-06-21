d21c05721bbdcb343611e84984a1fab148bf3a3b
Andrew <blayson4@gmail.com>
2018-06-21 13:24:05 +0200

    Update optimize-logos.sh

--- optimize-logos.sh
+++ optimize-logos.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-# loop through all folders with logos, convert and resize it
+ext='_small.png'
+
 cd themes/bootstrap3/images/institutions/logos/
+# loop through all folders with logos, convert and resize it
 for dir in */; do
   cd ${dir}
   for file in *; do
     if  [[ ${file} != *_*.png ]]; then
       ids=${file%%.*}
-      ext='_small.png'
       convert ${file} -strip -resize 80x60 ${ids}${ext}
     fi
   done
