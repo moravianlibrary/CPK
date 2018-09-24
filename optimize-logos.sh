dec7f34a1f9f9106824b519e1e6d2a9a973b153d
MartinKravec <kravec.martin@gmail.com>
2018-09-24 14:49:55 +0200

    ReOptimize all logos only with param. Do not optimize if already done.
    Add usage function ./optimize-logos.sh -h

--- optimize-logos.sh
+++ optimize-logos.sh
@@ -1,15 +1,42 @@
 #!/usr/bin/env bash
 
+function print_usage {
+    cat <<EOF
+
+USAGE: optimize-logos.sh [--all|-a] [--help|-h]
+
+  --all|-a          Optimize all files, do not skip already optimized files
+  --help|-h         Print usage
+
+EOF
+}
+
+optimize_all="false"
 ext='_small.png'
+
+while true ; do
+    case "$1" in
+         --all|-a)
+            optimize_all="true"
+            shift
+            ;;
+         --help|-h)
+            print_usage;
+            exit 0;;
+        *) break ;;
+    esac
+done
 
 cd themes/bootstrap3/images/institutions/logos/
 # loop through all folders with logos, convert and resize it
 for dir in */; do
   cd ${dir}
   for file in *; do
-    if  [[ ${file} != *_*.png ]]; then
-      ids=${file%%.*}
-      convert ${file} -strip -resize 80x60 ${ids}${ext}
+    if [[ ${file} != *_*.png ]]; then
+      file_name=${file%%.*}
+      if [[ ${optimize_all} == 'true' || (${optimize_all} == 'false' && ! -f ./${file_name}${ext}) ]]; then
+        convert ${file} -strip -resize 80x60 ${file_name}${ext}
+      fi
     fi
   done
   cd ..
