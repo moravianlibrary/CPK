8bf4c70d54043ce26c0cac9bf7770c525e039b25
MartinKravec <kravec.martin@gmail.com>
2018-11-19 13:30:49 +0100

    Code style refactoring

--- themes/bootstrap3/js/admin/widgets/widget-item.js
+++ themes/bootstrap3/js/admin/widgets/widget-item.js
@@ -4,7 +4,10 @@ jQuery( document ).ready( function( $ ) {
         prevText: 'Předchozí',
         nextText: 'Další',
         currentText: 'Hoy',
-        monthNames: ['Leden','Únor','Březen','Duben','Květen','Červen', 'Červenec','Srpen','Září','Říjen','Listopad','Prosinec'],
+        monthNames: [
+            'Leden','Únor','Březen','Duben','Květen','Červen',
+            'Červenec','Srpen','Září','Říjen','Listopad','Prosinec'
+        ],
         monthNamesShort: ['Le','Ún','Bř','Du','Kv','Čn', 'Čc','Sr','Zá','Ří','Li','Pr'],
         dayNames: ['Neděle','Pondělí','Úterý','Středa','Čtvrtek','Pátek','Sobota'],
         dayNamesShort: ['Ne','Po','Út','St','Čt','Pá','So',],
