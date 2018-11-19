48ed76c04b5bdcb47a95dc3abae22d49688eebb9
MartinKravec <kravec.martin@gmail.com>
2018-11-19 11:08:20 +0100

    Clear ./admin/widgets/infobox/create-item.phtml

---
+++ themes/bootstrap3/js/admin/widgets/widget-item.js
@@ -0,0 +1,30 @@
+jQuery( document ).ready( function( $ ) {
+    $.datepicker.regional['cs'] = {
+        closeText: 'Cerrar',
+        prevText: 'Předchozí',
+        nextText: 'Další',
+        currentText: 'Hoy',
+        monthNames: ['Leden','Únor','Březen','Duben','Květen','Červen', 'Červenec','Srpen','Září','Říjen','Listopad','Prosinec'],
+        monthNamesShort: ['Le','Ún','Bř','Du','Kv','Čn', 'Čc','Sr','Zá','Ří','Li','Pr'],
+        dayNames: ['Neděle','Pondělí','Úterý','Středa','Čtvrtek','Pátek','Sobota'],
+        dayNamesShort: ['Ne','Po','Út','St','Čt','Pá','So',],
+        dayNamesMin: ['Ne','Po','Út','St','Čt','Pá','So'],
+        weekHeader: 'Sm',
+        dateFormat: 'dd.mm.yy',
+        firstDay: 1,
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: ''
+    };
+
+    $.datepicker.setDefaults($.datepicker.regional['cs']);
+
+    $( '#dateFrom, #dateTo' ).datepicker( {
+        changeMonth: true,
+        changeYear: true,
+        dateFormat: 'yy-mm-dd'
+    });
+
+    CKEDITOR.replace('text_cs');
+    CKEDITOR.replace('text_en');
+});
