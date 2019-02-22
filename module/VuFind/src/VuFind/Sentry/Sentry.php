a1c2e0e41a2c7c89c38a79eb90737e64eb072b32
Vaclav Rosecky <xrosecky@gmail.com>
2019-02-22 08:30:18 +0100

    hotfix-1137: code style issues

--- module/VuFind/src/VuFind/Sentry/Sentry.php
+++ module/VuFind/src/VuFind/Sentry/Sentry.php
@@ -1,7 +1,43 @@
 <?php
-
+/**
+ * Sentry support.
+ *
+ * PHP version 5
+ *
+ * Copyright (C) Villanova University 2010.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @category VuFind2
+ * @package  Sentry
+ * @author   Vaclav Rosecky <xrosecky@mzk.cz>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://www.vufind.org  Main Page
+ */
 namespace Vufind\Sentry;
 
+/**
+ * Sentry support.
+ *
+ * Helper class for sending errors to sentry.io.
+ *
+ * @category VuFind2
+ * @package  Sentry
+ * @author   Vaclav Rosecky <xrosecky@mzk.cz>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://www.vufind.org  Main Page
+ */
 class Sentry {
 
     private static $errorHandler = null;
@@ -13,7 +49,8 @@ class Sentry {
             $sentryClient = new \Raven_Client($dsn);
             self::$errorHandler = new \Raven_ErrorHandler($sentryClient);
             self::$errorHandler->registerExceptionHandler();
-            $errorTypes = E_ERROR | E_PARSE | E_CORE_ERROR | E_CORE_WARNING | E_COMPILE_ERROR | E_COMPILE_WARNING | E_STRICT | E_RECOVERABLE_ERROR | E_DEPRECATED;
+            $errorTypes = E_ERROR | E_PARSE | E_CORE_ERROR | E_CORE_WARNING | E_COMPILE_ERROR | E_COMPILE_WARNING
+                | E_STRICT | E_RECOVERABLE_ERROR | E_DEPRECATED;
             self::$errorHandler->registerErrorHandler(true, $errorTypes);
             self::$errorHandler->registerShutdownFunction();
         }
