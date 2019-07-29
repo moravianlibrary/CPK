c7f2e5e50f070b54bf355deb926f41bfa93c6423
Robert Šípek <sipek@mzk.cz>
2019-07-29 15:06:21 +0200

    Disable all js and css

--- themes/bootstrap3/theme.config.php
+++ themes/bootstrap3/theme.config.php
@@ -5,38 +5,38 @@ $toRet = array(
         // 'vendor/bootstrap.min.css',
         // 'vendor/bootstrap-accessibility.css',
         // 'bootstrap-custom.css',
-        'compiled.css',
-        'vendor/font-awesome.min.css',
-        'vendor/bootstrap-slider.css',
-        'vendor/bootstrap-select.min.css',
-        'print.css:print',
-        'ol.css'
+//        'compiled.css',
+//        'vendor/font-awesome.min.css',
+//        'vendor/bootstrap-slider.css',
+//        'vendor/bootstrap-select.min.css',
+//        'print.css:print',
+//        'ol.css'
     ),
     'js' => array(
-        'vendor/base64.js:lt IE 10', // btoa polyfill
-        'vendor/jquery.min.js',
-        'vendor/bootstrap.min.js',
-        'vendor/rc4.js',
-        'vendor/js.cookie.js',
-        'vendor/bootstrap-datepicker.js',
-        'vendor/bootstrap-datepicker.cs.js',
-        'vendor/bootstrap-select.min.js',
-        'vendor/bootstrap-slider.js',
-        'vendor/jquery.validate.min.js',
-        'vendor/validation-additional-methods.js',
-        'autocomplete.js',
-        'vendor/validator.min.js',
-        'common.js',
-        'lightbox.js',
-        'eu-cookies.js',
-        'search-results.js',
-        'vendor/jsTree/jstree.min.js',
-        'facets.js',
-        'lz-string.js',
-        'obalkyknih.js',
-        'vendor/jquery.visible.min.js',
-        'vendor/jquery.bootstrap-growl.js',
-        'notifications.module.js',
+//        'vendor/base64.js:lt IE 10', // btoa polyfill
+//        'vendor/jquery.min.js',
+//        'vendor/bootstrap.min.js',
+//        'vendor/rc4.js',
+//        'vendor/js.cookie.js',
+//        'vendor/bootstrap-datepicker.js',
+//        'vendor/bootstrap-datepicker.cs.js',
+//        'vendor/bootstrap-select.min.js',
+//        'vendor/bootstrap-slider.js',
+//        'vendor/jquery.validate.min.js',
+//        'vendor/validation-additional-methods.js',
+//        'autocomplete.js',
+//        'vendor/validator.min.js',
+//        'common.js',
+//        'lightbox.js',
+//        'eu-cookies.js',
+//        'search-results.js',
+//        'vendor/jsTree/jstree.min.js',
+//        'facets.js',
+//        'lz-string.js',
+//        'obalkyknih.js',
+//        'vendor/jquery.visible.min.js',
+//        'vendor/jquery.bootstrap-growl.js',
+//        'notifications.module.js',
     ),
     'less' => array(
         'active' => false,
@@ -94,30 +94,30 @@ if (!($angularForbiddenForIE && USES_IE)) {
 
         $jsToInclude = [
 
-            'vendor/angular.min.js',
-
-            'ng-cpk/favorites/module.js',
-            'ng-cpk/favorites/favsNotifications.service.js',
-            'ng-cpk/favorites/favorite.class.js',
-            'ng-cpk/favorites/favorites.factory.js',
-            'ng-cpk/favorites/storage.service.js',
-            'ng-cpk/favorites/broadcaster.service.js',
-            'ng-cpk/favorites/list.controller.js',
-            'ng-cpk/favorites/record.controller.js',
-            'ng-cpk/favorites/search.controller.js',
-
-            'ng-cpk/federative-login/module.js',
-            'ng-cpk/federative-login/login.controller.js',
-
-            'ng-cpk/admin/module.js',
-            'ng-cpk/admin/configurations/conf.controller.js',
-
-            'ng-cpk/history/module.js',
-            'ng-cpk/history/checkedouthistory.controller.js',
-
-            'ng-cpk/module.js',
-            'ng-cpk/global.controller.js',
-            'ng-cpk/translate.filter.js'
+//            'vendor/angular.min.js',
+//
+//            'ng-cpk/favorites/module.js',
+//            'ng-cpk/favorites/favsNotifications.service.js',
+//            'ng-cpk/favorites/favorite.class.js',
+//            'ng-cpk/favorites/favorites.factory.js',
+//            'ng-cpk/favorites/storage.service.js',
+//            'ng-cpk/favorites/broadcaster.service.js',
+//            'ng-cpk/favorites/list.controller.js',
+//            'ng-cpk/favorites/record.controller.js',
+//            'ng-cpk/favorites/search.controller.js',
+//
+//            'ng-cpk/federative-login/module.js',
+//            'ng-cpk/federative-login/login.controller.js',
+//
+//            'ng-cpk/admin/module.js',
+//            'ng-cpk/admin/configurations/conf.controller.js',
+//
+//            'ng-cpk/history/module.js',
+//            'ng-cpk/history/checkedouthistory.controller.js',
+//
+//            'ng-cpk/module.js',
+//            'ng-cpk/global.controller.js',
+//            'ng-cpk/translate.filter.js'
         ];
 
         $toRet['js'] = array_merge($toRet['js'], $jsToInclude);
