09ad5e04e9bdac72f6b31ebc57bad1a8e8c1b3c1
sjiamnocna <janca@mzk.cz>
2019-09-11 13:12:38 +0200

    feature-1244: Added documentation + small changes in groups.php, preparing institution list

--- module/CPK/src/CPK/RecordDriver/Groups.php
+++ module/CPK/src/CPK/RecordDriver/Groups.php
@@ -11,33 +11,34 @@ namespace CPK\RecordDriver;
 
 class Groups
 {
-    public function getSortRecords($dedupedRecords) {
-
-        $tempCnbs = [];
-        $tempCaslins = [];
+    /**
+     * Sorts deduped records and excludes CNB and Caslins at the end
+     * @param $dedupedRecords   array   Array of deduped records
+     * @return array    Sorted deduped records
+     */
+    public static function getSortRecords($dedupedRecords) {
+        $temp['source_cnb'] = $temp['source_caslin'] = [];
         foreach ($dedupedRecords as $key => $record) {
-            if ($record['source'] === 'source_cnb') {
-                $tempCnbs[] = $record;
-                unset($dedupedRecords[$key]);
-            }
-            if ($record['source'] === 'source_caslin') {
-                $tempCaslins[] = $record;
+            //if source is one of these, exclude it and save temporary elsewhere
+            if (in_array($record['source'], ['source_cnb','source_caslin'])){
+                $temp[ $record['source'] ][] = $record;
                 unset($dedupedRecords[$key]);
             }
         }
         sort($dedupedRecords);
-        foreach ($tempCnbs as $key => $record) {
-            $dedupedRecords[] = $record;
-        }
-        foreach ($tempCaslins as $key => $record) {
-            $dedupedRecords[] = $record;
-        }
-
-        return $dedupedRecords;
+        //add CNBs and Caslins at the end of array, both are numeric indexed arrays
+        return array_merge($dedupedRecords, $temp['source_cnb'], $temp['source_caslin']);
     }
 
-    public function getInternalMultiplicity($dedupedRecords, $institution){
-        $records = array();
+    /**
+     * Get same institution from deduplicated list
+     * @param $dedupedRecords   Array of deduplicated results
+     * @param $institution
+     * @return array
+     */
+    public static function getInternalMultiplicity($dedupedRecords, $institution){
+        $records = [];
+        //get institution - e.g. 'source_mzk' => 'mzk'
         $compare = substr($institution,0, strpos($institution, '.'));
         foreach ($dedupedRecords as $key => $record) {
             if ($compare === substr($record['source'], 7) && $institution !== $record['id']) {
