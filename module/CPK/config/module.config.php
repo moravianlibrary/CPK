e70600fdc29c1294b17ccd7a7921d1cf6f3bef24
Josef Moravec <josef.moravec@gmail.com>
2019-11-21 12:49:45 +0100

    feature-1107: Fix merge mistake

--- module/CPK/config/module.config.php
+++ module/CPK/config/module.config.php
@@ -232,7 +232,7 @@ $config = array(
             'CPK\NotificationsHandler' => 'CPK\Notifications\Factory::getNotificationsHandler',
             'CPK\Mailer' => 'CPK\Mailer\Factory::createService',
             'VuFind\ILSHoldLogic' => 'CPK\ILS\Logic\Factory::getFlatHolds',
-            'Mzk\ZiskejApi\Api' => 'CPK\ZiskejApiFactory'
+            'Mzk\ZiskejApi\Api' => 'CPK\ZiskejApiFactory',
             'CPK\KohaOAUTH2Service' => 'CPK\Auth\Factory::getKohaOAUTH2Service',
         ), // Exceptions throwing system
 
