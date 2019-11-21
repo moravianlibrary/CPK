a669d1a23d58dc89fca8bc511886a8d3d7b2e84d
Josef Moravec <josef.moravec@gmail.com>
2019-11-21 14:00:01 +0100

    feature-1107: Clean up Koha rest normalizer

--- module/CPK/src/CPK/ILS/Logic/KohaRestNormalizer.php
+++ module/CPK/src/CPK/ILS/Logic/KohaRestNormalizer.php
@@ -1,23 +1,61 @@
 <?php
-
+/**
+ * Data normalizer for Koha REST API ILS driver
+ *
+ * PHP version 7
+ *
+ * Copyright (C) Moravian Library 2019.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @author   Josef Moravec <moravec@mzk.cz>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ */
 namespace CPK\ILS\Logic;
 
 use \VuFind\Date\Converter as DateConverter;
 
 class KohaRestNormalizer
 {
-        protected $dateConverter;
+    /**
+     * Date converter object
+     *
+     * @var DateConverter
+     */
+    protected $dateConverter;
 
+    /**
+     * KohaRestNormalizer constructor.
+     *
+     * @param DateConverter $converter
+     */
     public function __construct(DateConverter $converter)
     {
         $this->dateConverter = $converter;
     }
 
-    public function normalize($response, $methodName) {
+    /**
+     * normalize - main normalization method
+     *
+     * @param array $response    JSON decoded result
+     * @param string $methodName ILS Driver method
+     *
+     * @return array normalized response
+     */
+    public function normalize($response, $methodName)
+    {
         switch ($methodName) {
-            case 'getMyProfile':
-                $this->normalizeUserProfileResponse($response);
-                break;
             case 'getMyFines':
                 $this->normalizeUserFinesResponse($response);
                 break;
@@ -27,34 +65,16 @@ class KohaRestNormalizer
             case 'getMyTransactions':
                 $this->normalizeCheckoutsResponse($response);
                 break;
-            case 'renewMyItems':
-                $this->normalizeRenewItem($response);
-                break;
         }
-
         return $response;
     }
 
-    public function normalizeUserProfileResponse(&$response) {
-        $response['expiry_date'] = !empty($response['expiry_date'])
-            ? $this->dateConverter->convertToDisplayDate(
-                'Y-m-d', $response['expiry_date']
-            ) : '';
-    }
-
-    public function normalizeUserFinesResponse(&$response) {
+    private function normalizeUserFinesResponse(&$response)
+    {
         foreach ($response['outstanding_debits']['lines'] as $key => $entry) {
-            if ($entry['account_type'] == 'Pay'
-                || (int)$entry['amount_outstanding'] == 0
-            ) {
-                unset($response['outstanding_debits']['lines'][$key]);
-                continue;
-            }
-
             $entry['date'] = !empty($entry['date'])
                 ? $this->normalizeDate($entry['date'])
                 : '';
-
             $entry['amount'] *= 100;
             $entry['amount_outstanding'] *= 100;
             $entry['description'] = trim($entry['description']);
@@ -63,7 +83,7 @@ class KohaRestNormalizer
         }
     }
 
-    public function normalizeHoldItemsResponse(&$response) {
+    private function normalizeHoldItemsResponse(&$response) {
         foreach ($response as $key => $entry) {
             $entry['hold_date'] = !empty($entry['hold_date'])
                 ? $this->normalizeDate($entry['hold_date'])
@@ -76,9 +96,10 @@ class KohaRestNormalizer
         }
     }
 
-    public function normalizeCheckoutsResponse(&$response) {
+    private function normalizeCheckoutsResponse(&$response)
+    {
         foreach ($response as $key => $entry) {
-            $entry['item_id'] = isset($entry['item_id']) ? $entry['item_id'] : null;
+            $entry['item_id'] = $entry['item_id'] ?? null;
             $entry['due_date'] = !empty($entry['due_date'])
                 ? $this->normalizeDate($entry['due_date'])
                 : '';
@@ -92,13 +113,8 @@ class KohaRestNormalizer
         }
     }
 
-    public function normalizeRenewItem(&$response) {
-        $response['due_date'] = !empty($response['due_date'])
-            ? $this->normalizeDate($response['due_date'])
-            : '';
-    }
-
-    public function normalizeDate($date, $with_time = false) {
+    public function normalizeDate($date, $with_time = false)
+    {
         $create_format = $with_time ? 'c': 'Y-m-d';
         return $this->dateConverter->convertToDisplayDate($create_format, $date);
     }
