c930e42b63b5d1c460ec7f56cda1359288efe7fa
sjiamnocna <janca@mzk.cz>
2019-12-13 16:50:28 +0100

    feature-1250: Working for idp

--- themes/bootstrap3/js/common.js
+++ themes/bootstrap3/js/common.js
@@ -772,51 +772,69 @@ $.fn.listSearch = function (param) {
         inputClass: param.inputClass !== undefined ? param.inputClass : 'search-term',
         placeholder: param.placeholder !== undefined ? param.placeholder : 'Write query'
     };
-    let that = $(this);
+    let that = $(this), i=1;
+
     this.input = $('<input>').addClass(param.inputClass).prop('placeholder', param.placeholder).on("keyup", function (e) {
         // value of text field, count of results
         let value = $(this).val().toLowerCase(), resultCount = 0;
         that.find(param.itemType).filter(function () {
             if ($(this).data('search').toLowerCase().indexOf(value) > -1) {
-                $(this).show();
+                $(this).addClass('result');
                 resultCount++;
             } else {
-                $(this).hide();
-                // IC the current elem is active one
-                $(this).removeClass('active');
+                // IC the current elem is active or was one of previous results
+                $(this).removeClass('active result');
             }
         });
 
         if (resultCount > 0) {
             // move in the searched list
-            let activeEl = that.find(param.itemType + '.active'),
-                elList = that.find(param.itemType + ':visible'),
-                nextEl;
+            let
+                elList = that.find(param.itemType + '.result'),
+                activeEl = that.find(param.itemType + '.active'), nextEl, prevEl;
 
             if (activeEl.length == 0) {
                 // no active item, now select the first visible element as active
                 elList.first().addClass('active');
             } else {
+                // begin from 2nd element, end before last elem
+                for (i = 0; i < elList.length; i++) {
+                    if ($(elList[i]).hasClass('active')) {
+                        // normal elem
+                        prevEl = elList[i - 1] !== undefined ? $(elList[i - 1]) : 0;
+                        activeEl = $(elList[i]);
+                        // assign only if there is any ancestor
+                        nextEl = elList[i + 1] !== undefined ? $(elList[i + 1]) : 0;
+
+                        // don't need to go further
+                        break;
+                    }
+                    console.log(prevEl);
+                    console.log(nextEl);
+                }
+
                 // one of previous keypress created active el, now can move in the list
                 if ([13, 38, 40].indexOf(e.keyCode) > -1) {
                     // one of these keys pressed
                     switch (e.keyCode) {
                         case 38: // up
-                            nextEl = activeEl.prev(param.itemType + ':visible');
-                            if (nextEl.length > 0) {
+                            if (prevEl != 0) {
                                 activeEl.removeClass('active');
-                                nextEl.addClass('active');
+                                prevEl.addClass('active');
                             }
                             break;
                         case 40: // down
-                            nextEl = activeEl.next(param.itemType + ':visible');
-                            if (nextEl.length > 0) {
+                            if (nextEl != 0) {
                                 activeEl.removeClass('active');
                                 nextEl.addClass('active');
                             }
                             break;
                         case 13: // enter
                             // simulate click event
+                            if (param.clickSelector != undefined){
+                                // if target element is child of active one
+                                activeEl = activeEl.find(param.clickSelector);
+                            }
                             activeEl.click();
                             break;
                     }
@@ -850,7 +868,8 @@ $.fn.dropFinder = function (placeholder) {
     let dropdownList = $(this).find('.dropdown-menu').listSearch({
         itemType: 'li',
         inputClass: 'dropdown-search',
-        placeholder: placeholder
+        placeholder: placeholder,
+        clickSelector: 'a'
     });
     $(this).on('shown.bs.dropdown', function () {
         dropdownList.focus();
