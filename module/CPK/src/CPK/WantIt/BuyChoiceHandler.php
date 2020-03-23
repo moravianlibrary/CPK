1ce4374b099a90c9a87e8178c8c2c9df974b13d6
Josef Moravec <josef.moravec@gmail.com>
2020-03-23 11:13:13 +0100

    feature-1306: Reformat code

--- module/CPK/src/CPK/WantIt/BuyChoiceHandler.php
+++ module/CPK/src/CPK/WantIt/BuyChoiceHandler.php
@@ -19,98 +19,102 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * @author	Martin Kravec	<kravec@mzk.cz>
- * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @author    Martin Kravec    <kravec@mzk.cz>
+ * @license   http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  */
-namespace CPK\WantIt;
 
-use CPK\WantIt\BuyChoiceHandlerInterface;
-use CPK\WantIt\AbstractHttpClient;
+namespace CPK\WantIt;
 
 /**
  * BuyChoiceHandler Model
  * Provides available Buy actions on records and handles user's requests.
  *
- * @author	Martin Kravec	<kravec@mzk.cz>
- * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @author    Martin Kravec    <kravec@mzk.cz>
+ * @license   http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  */
-class BuyChoiceHandler extends AbstractHttpClient implements BuyChoiceHandlerInterface
+class BuyChoiceHandler extends AbstractHttpClient implements
+    BuyChoiceHandlerInterface
 {
-	/**
-	 * RecordDriver
-	 * @var	\CPK\RecordDriver\SolrMarc	$recordDriver
-	 */
-	protected $recordDriver;
+    /**
+     * RecordDriver
+     *
+     * @var \CPK\RecordDriver\SolrMarc $recordDriver
+     */
+    protected $recordDriver;
 
-	public function __construct(\CPK\RecordDriver\SolrMarc $recordDriver)
-	{
-		$this->recordDriver = $recordDriver;
-	}
+    public function __construct(\CPK\RecordDriver\SolrMarc $recordDriver)
+    {
+        $this->recordDriver = $recordDriver;
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function getGoogleBooksVolumeLink()
-	{
-        $isbn = (!empty($isbns = $this->recordDriver->getISBNs())) ? $isbns[0] : null;
-		$lccn = $this->recordDriver->getLCCN();
-		$oclc = $this->recordDriver->getCleanOCLCNum();
+    /**
+     * {@inheritdoc}
+     */
+    public function getGoogleBooksVolumeLink()
+    {
+        $isbns = $this->recordDriver->getISBNs();
+        $isbn = $isbns[0] ?? null;
+        $lccn = $this->recordDriver->getLCCN();
+        $oclc = $this->recordDriver->getCleanOCLCNum();
 
-		if (empty($isbn) && empty($lccn) && empty($oclc)) {
+        if (empty($isbn) && empty($lccn) && empty($oclc)) {
             return null;
         }
 
-		$url = 'https://www.googleapis.com/books/v1/volumes';
+        $url = 'https://www.googleapis.com/books/v1/volumes';
 
-		if (!empty($isbn)) {
+        if (!empty($isbn)) {
             $params = ['q' => 'isbn:' . str_replace("-", "", $isbn)];
         }
-		if (!empty($lccn)) {
+        if (!empty($lccn)) {
             $params = ['q' => 'lccn:' . $lccn];
         }
-		if (!empty($oclc)) {
+        if (!empty($oclc)) {
             $params = ['q' => 'oclc:' . $oclc];
         }
 
-		$dataArray = $this->getRequestDataResponseAsArray($url, $params);
+        $dataArray = $this->getRequestDataResponseAsArray($url, $params);
 
-		$link = null;
-		if($dataArray['totalItems'] >= 1) {
-            $link = $dataArray['items'][0]['volumeInfo']['canonicalVolumeLink'] ?? null;
+        $link = null;
+        if ($dataArray['totalItems'] >= 1) {
+            $link = $dataArray['items'][0]['volumeInfo']['canonicalVolumeLink'] ??
+                null;
         }
 
-		return $link;
-	}
+        return $link;
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	public function getZboziLink()
+    /**
+     * {@inheritdoc}
+     */
+    public function getZboziLink()
     {
-        $isbn = (!empty($isbns = $this->recordDriver->getISBNs())) ? $isbns[0] : null;
+        $isbns = $this->recordDriver->getISBNs();
+        $isbn = $isbns[0] ?? null;
 
         if (empty($isbn)) {
             return null;
         }
 
-		$url = 'https://www.zbozi.cz/api/v1/search';
-		$params = [
-		    'groupByCategory' => 0,
+        $url = 'https://www.zbozi.cz/api/v1/search';
+        $params = [
+            'groupByCategory' => 0,
             'loadTopProducts' => 'true',
             'page' => 1,
             'query' => str_replace("-", "", $isbn),
-		];
+        ];
 
-		$dataArray = $this->getRequestDataResponseAsArray($url, $params);
+        $dataArray = $this->getRequestDataResponseAsArray($url, $params);
 
-		$link = null;
+        $link = null;
 
-		if (isset($dataArray['status']) && $dataArray['status'] === 200) {
+        if (isset($dataArray['status']) && $dataArray['status'] === 200) {
             $productUrl = $dataArray['products'][0]['normalizedName'] ?? null;
             if ($productUrl !== null) {
-                $link = 'https://www.zbozi.cz/vyrobek/' . urlencode($productUrl) . '/';
+                $link = 'https://www.zbozi.cz/vyrobek/' . urlencode($productUrl)
+                    . '/';
             }
         }
-		return $link;
-	}
+        return $link;
+    }
 }
