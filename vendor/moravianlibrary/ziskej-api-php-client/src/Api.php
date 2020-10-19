50f6e6b965f06dffcc3f0d3dada90c55aa6824df
Robert Sipek <sipek@mzk.cz>
2020-10-19 13:19:06 +0200

    issue-228: Create method getLibrariesAll

--- vendor/moravianlibrary/ziskej-api-php-client/src/Api.php
+++ vendor/moravianlibrary/ziskej-api-php-client/src/Api.php
@@ -76,6 +76,47 @@ final class Api
      */
 
     /**
+     * List all libraries
+     * GET /libraries
+     *
+     * @return \Mzk\ZiskejApi\ResponseModel\LibraryCollection
+     *
+     * @throws \Mzk\ZiskejApi\Exception\ApiResponseException
+     * @throws \Http\Client\Exception
+     */
+    public function getLibrariesAll(): LibraryCollection
+    {
+        $apiResponse = $this->apiClient->sendApiRequest(
+            new ApiRequest(
+                'GET',
+                '/libraries',
+                [],
+                [
+                    'service' => 'mvszk',
+                    'include_deactivated' => 1,
+                ],
+
+            )
+        );
+
+        switch ($apiResponse->getStatusCode()) {
+            case 200:
+                $contents = $apiResponse->getBody()->getContents();
+                $array = json_decode($contents, true);
+
+                if (isset($array['items']) && is_array($array['items'])) {
+                    return LibraryCollection::fromArray($array['items']);
+                } else {
+                    return new LibraryCollection();
+                }
+                break;
+            default:
+                throw new \Mzk\ZiskejApi\Exception\ApiResponseException($apiResponse);
+                break;
+        }
+    }
+
+    /**
      * List all active libraries
      * GET /libraries
      *
