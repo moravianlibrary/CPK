7046951b72269247db5ac085f65c3d08fb632a82
Robert Sipek <sipek@mzk.cz>
2020-10-19 13:21:38 +0200

    issue-228: Use method getLibrariesAll

--- module/CPK/src/CPK/Controller/AjaxController.php
+++ module/CPK/src/CPK/Controller/AjaxController.php
@@ -677,7 +677,7 @@ class AjaxController extends AjaxControllerBase
         /** @var \CPK\ILS\Driver\MultiBackend $cpkMultibackend */
         $cpkMultibackend = $this->getILS()->getDriver();
 
-        $ziskejLibs = $ziskejApi->getLibrariesActive()->getAll();
+        $ziskejLibs = $ziskejApi->getLibrariesAll()->getAll();
         $libraryIds = [];
         foreach ($ziskejLibs as $ziskejLib) {
             $libraryId = $cpkMultibackend->siglaToSource($ziskejLib->getSigla());
